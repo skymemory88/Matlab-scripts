fd8667ab1e6b1dd79bead74cc0d96c93d24b1a23
skymemory88 <skymemory88@hotmail.com>
2020-03-25 14:53:54 -0600

    first commit

---
+++ Plot functions/Fieldscan/functions/S11_sim.m
@@ -0,0 +1,20 @@
+w = linspace(min(f0),max(f0),500);
+w = w';
+B = linspace(min(H0),max(H0),500);
+kc = 0.005;
+ke = 0.005;
+gc = fitPara.g;
+wc = fitPara.wc;
+Br = fitPara.x0;
+gamma = 3*fitPara.gamma;
+hbar = 1.0;
+Delt = 7/2*(B-Br)/hbar;
+S11 = abs(1+ke./(1i.*(w-wc)-kc+gc^2./(1i.*Delt-gamma)));
+surf(B, w, log(S11),'edgecolor','none');
+xlim([min(B) max(B)]);
+ylim([min(w) max(w)]);
+xlabel('Magnetic field (T)');
+ylabel('Frequency (GHz)');
+title('Simulated S11 response');
+colorbar
+view(0,90);
