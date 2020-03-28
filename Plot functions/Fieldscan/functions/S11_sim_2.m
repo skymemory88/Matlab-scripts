61a896ba12cb727ad312d763e788e3604f171b7c
skymemory88 <skymemory88@hotmail.com>
2020-03-27 20:17:29 -0600

    script cleaning

--- Plot functions/Fieldscan/functions/S11_sim_2.m
+++ Plot functions/Fieldscan/functions/S11_sim_2.m
@@ -1,23 +1,39 @@
-delete(splot)
+% % delete(splot)
+% % delete(lplot)
 hold on
-% gc = 0.3;
-gp = 0.1;
-gm = 0.3;
-QPT = 4.9;
-f_cav = 3.53;
-B_l = fields(fields <= QPT);
-B_h = fields(fields > QPT);
-% wmm = min(Ediff(:,:));
-% wpp = Ediff(:,:);
-% wpp = Ediff(6,1:numel(B_l));
-% wpp2 = Ediff(6,numel(B_l)+1:end);
-wmm = Ediff(1,1:numel(B_l));
-wmm2 = Ediff(6,numel(B_l)+1:end);
-% wp = 0.5.*(wpp + f_cav)+0.5.*sqrt((wpp-f_cav).^2 + gm^2);
-% wp2 = 0.5.*(wpp2 + f_cav)+0.5.*sqrt((wpp2-f_cav).^2 + gp^2);
-wm = 0.5.*(wmm + f_cav)-0.5.*sqrt((wmm-f_cav).^2 + gm^2);
-wm2 = 0.5.*(wmm2 + f_cav)-0.5.*sqrt((wmm2-f_cav).^2 + gp^2);
-% level = plot(fields,Ediff,'-k');
-% splot = plot(fields,wp,'-r',fields,wm,'-r','linewidth',2);
-% splot = plot(B_l,wm,'-r',B_h,wm2,'-b',B_l,wp,'-k',B_h,wp2,'-k','Linewidth',2);
-splot = plot(B_l,wm,'-r',B_h,wm2,'-r','Linewidth',2);
+gc = 0.055;
+f_cav = 3.54;
+wmm = min(Ediff(:,:)); % choose the groud state as the lowest in energy
+wpp = max(Ediff(:,:)); % choose the highest occupied level
+wp = 0.5.*(wpp + f_cav)+0.5.*sqrt((wpp-f_cav).^2 + gc^2);
+wm = 0.5.*(wmm + f_cav)-0.5.*sqrt((wmm-f_cav).^2 + gc^2);
+lplot = plot(fields,Ediff(:,:),':k','linewidth',2);
+hold on
+splot = plot(fields,wp,'-r',fields,wm,'-r','linewidth',2);
+xlim([min(fields) max(fields)])
+xlabel('Magnetic field (T)')
+ylabel('Energy (GHz)')
+%% Two parts simulation 
+% % In the case of strong coupling, the coupling strength change before and
+% % afterthe quantum transition point (QTP), therefore split the curve into
+% % two partes before and after the QTP.
+% % 
+% delete(splot)
+% hold on
+% f_cav = 3.57;
+% gc_f = 0.4;
+% gc_p = 0.25;
+% QTP = 4.9;
+% B_l = fields(fields <= QTP);
+% B_h = fields(fields > QTP);
+% % wpp = Ediff(1,1:numel(B_l));
+% % wpp2 = Ediff(7,numel(B_l)+1:end);
+% wmm = Ediff(1,1:numel(B_l));
+% wmm2 = Ediff(7,numel(B_l)+1:end);
+% % wp = 0.5.*(wpp + f_cav)+0.5.*sqrt((wpp-f_cav).^2 + gc_f^2);
+% % wp2 = 0.5.*(wpp2 + f_cav)+0.5.*sqrt((wpp2-f_cav).^2 + gc_p^2);
+% wm = 0.5.*(wmm + f_cav)-0.5.*sqrt((wmm-f_cav).^2 + gc_f^2);
+% wm2 = 0.5.*(wmm2 + f_cav)-0.5.*sqrt((wmm2-f_cav).^2 + gc_p^2);
+% % lplot = plot(fields,Ediff,'-k');
+% % splot = plot(B_l,wm,'-r',B_h,wm2,'-b',B_l,wp,'-k',B_h,wp2,'-k','Linewidth',2);
+% splot = plot(B_l,wm,'-r',B_h,wm2,'-b','Linewidth',2);
--- Plot functions/Fieldscan/functions/S11_sim_strg.m
+++ Plot functions/Fieldscan/functions/S11_sim_strg.m
@@ -6,8 +6,14 @@ wc = fitPara.wc;
 Br = fitPara.x0;
 gamma = fitPara.gamma;
 hbar = 1.0;
-Delt = 7/2.*(B-Br)./hbar;
-S11 = abs(1+ke./(1i.*(w-wc)-kc+gc^2./(1i.*Delt-gamma)));
+Delt = -7/2.*(B(1,:)-Br)./hbar;
+wp = wc + 0.5.*Delt + 0.5.*sqrt(Delt.^2 + 4*gc^2);
+wm = wc + 0.5.*Delt - 0.5.*sqrt(Delt.^2 + 4*gc^2);
+hold on
+p1 = plot(B(1,:),wp,'-r',B(1,:),wm,'-r','Linewidth',1);
+
+figure
+S11 = abs(1+ke./(1i.*(w-wc)-kc+gc^2./(1i.*(-Delt)-gamma)));
 surf(B, w, log(S11),'edgecolor','none');
 xlim([min(H0) max(H0)]);
 ylim([min(f0) max(f0)]);
