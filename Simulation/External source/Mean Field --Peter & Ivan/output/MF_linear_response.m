6cf1ae44eda1a51687770f9ba07b19c38eec4328
skymemory88 <skymemory88@hotmail.com>
2020-04-05 17:45:22 -0600

    remove temperature dimension (save data from each temperature in individual file)

--- Simulation/External source/Mean Field --Peter & Ivan/output/MF_linear_response.m
+++ Simulation/External source/Mean Field --Peter & Ivan/output/MF_linear_response.m
@@ -1,141 +1,129 @@
 function MF_linear_response
 cd('G:\My Drive\File sharing\Programming scripts\Matlab\Simulation\External source\Mean Field --Peter & Ivan\output')
 clearvars;
-Temperatures = [0.100, 0.150, 0.200, 0.250, 0.300, 0.350];
-% Temperatures = 0.150;
-for ii = 1:length(Temperatures)
-    filenames = strcat(num2str(Temperatures(ii),'%.3f'),'.mat');
-    load(filenames,'-mat','eee','fff','ttt','vvv'); % loads variables "fields", "temp", "E" and "V" 
-    % which are eigenstates and eigenvalues calculated in the mean-field model 
-    % as a function of transverse field and temperature
-    linear_response(eee,fff,ttt,vvv);
-end
+% Temperatures = [0.250, 0.300, 0.350, 0.500];
+Temperatures = [0.450 0.800];
+    for ii = 1:length(Temperatures)
+        filenames = strcat(num2str(Temperatures(ii),'%.3f'),'.mat');
+        load(filenames,'-mat','eee','fff','ttt','vvv'); % loads variables "fields", "temp", "E" and "V" 
+        % which are eigenstates and eigenvalues calculated in the mean-field model 
+        % as a function of transverse field and temperature
+        linear_response(eee,fff,ttt,vvv);
+    end
 end
 function linear_response(eee,fff,ttt,vvv)
 %% Calculation of susceptibilities
 E = eee;
 V = vvv;
-temp = ttt;
 fields = vecnorm(fff);
 freq_total = (1:0.02:5);
 
-imchix = double.empty(length(temp(1,:)),length(freq_total(1,:)),length(fields(1,:)),0);
-imchiy = double.empty(length(temp(1,:)),length(freq_total(1,:)),length(fields(1,:)),0);
-imchiz = double.empty(length(temp(1,:)),length(freq_total(1,:)),length(fields(1,:)),0);
+imchix = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+imchiy = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+imchiz = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
 
-rechi1x = double.empty(length(temp(1,:)),length(freq_total(1,:)),length(fields(1,:)),0);
-rechi1y = double.empty(length(temp(1,:)),length(freq_total(1,:)),length(fields(1,:)),0);
-rechi1z = double.empty(length(temp(1,:)),length(freq_total(1,:)),length(fields(1,:)),0);
+rechi1x = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+rechi1y = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+rechi1z = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
 
-for l = 1:length(temp(1,:)) % calculate susceptibility for all temperatures
-    tt = temp(1,l);
+for m = 1:length(freq_total(1,:)) %calculate susceptibility for all frequencies
+    freq = freq_total (m);
+    J=8;
+    I=3.5;
+    gLande_Ho=1.25;
 
-    for m = 1:length(freq_total(1,:)) %calculate susceptibility for all frequencies
-        freq = freq_total (m);
-        J=8;
-        I=3.5;
-        gLande_Ho=1.25;
+    %Initiate J operators
+    Jz=diag(J:-1:-J); % Jz = -J, -J+1,...,J-1,J
+    Jzh=kron(Jz,eye(2*I+1)); % Expand Jz space to include nuclear degree of freedom
+    Jp=diag(sqrt((J-((J-1):-1:-J) ).*(J+1+( (J-1):-1:-J) )),1); % electronic spin ladder operator
+    Jm=Jp'; % electronic spin ladder operator
+    Jph=kron(Jp,eye(2*I+1)); % Expand Hilbert space
+    Jmh=kron(Jm,eye(2*I+1)); % Expand Hilbert space
+    Jxh=(Jph+Jmh)/2;
+    Jyh=(Jph-Jmh)/2i;
+    %tensor product of cristal field to include nuclear moments
+    %Initiate I operators
+    Iz=diag(I:-1:-I); %Iz = -I, -I+1,...,I-1,I
+    Izh=kron(eye(2*J+1),Iz); % Expand Hilbert space
+    Ip=diag(sqrt((I-[(I-1):-1:-I]).*(I+1+[(I-1):-1:-I])),1); % Nuclear spin ladder operator
+    Im=Ip'; % Nuclear spin ladder operator
+    Iph=kron(eye(2*J+1),Ip); % Expand to match the Hilbert space
+    Imh=kron(eye(2*J+1),Im); % Expand to match the Hilbert space
+    Ixh=(Iph+Imh)/2;
+    Iyh=(Iph-Imh)/2i;
 
-        %Initiate J operators
-        Jz=diag(J:-1:-J); % Jz = -J, -J+1,...,J-1,J
-        Jzh=kron(Jz,eye(2*I+1)); % Expand Jz space to include nuclear degree of freedom
-        Jp=diag(sqrt((J-((J-1):-1:-J) ).*(J+1+( (J-1):-1:-J) )),1); % electronic spin ladder operator
-        Jm=Jp'; % electronic spin ladder operator
-        Jph=kron(Jp,eye(2*I+1)); % Expand Hilbert space
-        Jmh=kron(Jm,eye(2*I+1)); % Expand Hilbert space
-        Jxh=(Jph+Jmh)/2;
-        Jyh=(Jph-Jmh)/2i;
-        %tensor product of cristal field to include nuclear moments
-        %Initiate I operators
-        Iz=diag(I:-1:-I); %Iz = -I, -I+1,...,I-1,I
-        Izh=kron(eye(2*J+1),Iz); % Expand Hilbert space
-        Ip=diag(sqrt((I-[(I-1):-1:-I]).*(I+1+[(I-1):-1:-I])),1); % Nuclear spin ladder operator
-        Im=Ip'; % Nuclear spin ladder operator
-        Iph=kron(eye(2*J+1),Ip); % Expand to match the Hilbert space
-        Imh=kron(eye(2*J+1),Im); % Expand to match the Hilbert space
-        Ixh=(Iph+Imh)/2;
-        Iyh=(Iph-Imh)/2i;
+    ghztomeV = 1/241.8;
+    omega = freq*ghztomeV;     % define frequency sweep range (meV)
+    gama = 0.00005; % define lifetime (meV) (theoretical basis?)
 
-        ghztomeV = 1/241.8;
-        omega = freq*ghztomeV;     % define frequency sweep range (meV)
-        gama = 0.00005; % define lifetime (meV) (theoretical basis?)
+    for k = 1:length(fields(1,:)) % calculate susceptibility for all fields
+        v = squeeze ( squeeze(V(k,:,:,:)) ); % Obtain the corresponding eigen vectors
+        en = squeeze ( squeeze(E(k,:,:)) ); % Obtain the corresponding eigen energies in meV
+%       N = length(en);
+%       chi_t = zeros(1,N^2);
+%       ll = 1;
+        beta = 1/(ttt*8.617E-2); %[meV^-1]
+        z=sum(exp(-beta*en));
+        zz=exp(-beta*en)/z;
+        [n,np]=meshgrid(zz,zz);
+        NN=n-np;
+        [ee,eep]=meshgrid(en,en);
+        EE = eep-ee-omega;
+        gamma = ones(size(EE))*gama;
+        G = gamma ./ (EE.^2 + gamma.^2); 
+        G1 = EE ./ (EE.^2 + gamma.^2);  
 
-        for k = 1:length(fields(1,:)) % calculate susceptibility for all fields
-            v = squeeze ( V(k,l,:,:) ); % Obtain the corresponding eigen vectors
-            en = squeeze ( E(k,l,:) ); % Obtain the corresponding eigen energies in meV
-%             field = fields(1,k); % Obtain the corresponding field
+        ELEf = gLande_Ho * 0.05788;     % Lande factor x Bohr magneton (meV T^-1)
+        NUCf = 4.732 * 3.1519e-5;   % Nuclear Lande factor
+        JxhT = Jxh * ELEf;
+        IxhT = Ixh * NUCf;
 
-%             N = length(en);
-%             chi_t = zeros(1,N^2);
-            %ll = 1;
-%             zz = double.empty(0,N);
-%             beta = 1/(tt/11.6);
-            beta = 1/(tt*8.617E-2); %[meV^-1]
-            z=sum(exp(-beta*en));
-            zz=exp(-beta*en)/z;
-            [n,np]=meshgrid(zz,zz);
-            NN=n-np;
-            [ee,eep]=meshgrid(en,en);
-%             EE1=1./(ee-eep-omega);
-            EE = eep-ee-omega;
-            gamma = ones(size(EE))*gama;
-            G = gamma ./ (EE.^2 + gamma.^2);
-            G1 = EE ./ (EE.^2 + gamma.^2);
+        JyhT = Jyh * ELEf;
+        IyhT = Iyh * NUCf;
 
-            ELEf = gLande_Ho * 0.05788;     % Lande factor x Bohr magneton (meV T^-1)
-            NUCf = 4.732 * 3.1519e-5;   % Nuclear Lande factor
-            JxhT = Jxh * ELEf;
-            IxhT = Ixh * NUCf;
-            
-            JyhT = Jyh * ELEf;
-            IyhT = Iyh * NUCf;
-            
-            JzhT = Jzh * ELEf;
-            IzhT = Izh * NUCf;
+        JzhT = Jzh * ELEf;
+        IzhT = Izh * NUCf;
 
 % Calculate susceptibilities along x-axis
-            ttx  = v'  * (JxhT+IxhT) * v; 
-            chi_tx  = (ttx) .* (ttx.') .* NN .* G;
-            chi_t1x = (ttx) .* (ttx.') .* NN .* G1;
-            sss=sum(sum(chi_tx)); 
-            sss1=sum(sum(chi_t1x));
-            imchix  (l,m,k,1) =  real(sss)   ;
-            rechi1x (l,m,k,1) =  real(sss1)  ; 
+        ttx  = v'  * (JxhT+IxhT) * v; 
+        chi_tx  = (ttx) .* (ttx.') .* NN .* G;
+        chi_t1x = (ttx) .* (ttx.') .* NN .* G1;
+        sss=sum(sum(chi_tx)); 
+        sss1=sum(sum(chi_t1x));
+        imchix  (m,k,1) =  real(sss)   ;
+        rechi1x (m,k,1) =  real(sss1)  ; 
 
 % Calculate susceptibilities along y-axis
-            tty  = v'  * (JyhT+IyhT) * v; 
-            chi_ty  = (tty) .* (tty.') .* NN .* G;
-            chi_t1y = (tty) .* (tty.') .* NN .* G1;
-            sss=sum(sum(chi_ty));
-            sss1=sum(sum(chi_t1y));
-            imchiy  (l,m,k,1) =  real(sss)   ;
-            rechi1y (l,m,k,1) =  real(sss1)  ;   
-%             titttz = 'S(Jyy+Iyy)';
+        tty  = v'  * (JyhT+IyhT) * v; 
+        chi_ty  = (tty) .* (tty.') .* NN .* G;
+        chi_t1y = (tty) .* (tty.') .* NN .* G1;
+        sss=sum(sum(chi_ty));
+        sss1=sum(sum(chi_t1y));
+        imchiy  (m,k,1) =  real(sss)   ;
+        rechi1y (m,k,1) =  real(sss1)  ;   
 
 % Calculate susceptibilities along c-axis
-            ttz  = v'  * (JzhT+IzhT) * v;
-            chi_tz  = (ttz) .* (ttz.') .* NN .* G;
-            chi_t1z = (ttz) .* (ttz.') .* NN .* G1;
-            sss=sum(sum(chi_tz));
-            sss1=sum(sum(chi_t1z));
-            imchiz  (l,m,k,1) =  real(sss)   ;
-            rechi1z (l,m,k,1) =  real(sss1)  ;
-            %titttz = 'S(Jzz+Izz)';
-        end
+        ttz  = v'  * (JzhT+IzhT) * v;
+        chi_tz  = (ttz) .* (ttz.') .* NN .* G;
+        chi_t1z = (ttz) .* (ttz.') .* NN .* G1;
+        sss=sum(sum(chi_tz));
+        sss1=sum(sum(chi_t1z));
+        imchiz  (m,k,1) =  real(sss)   ;
+        rechi1z (m,k,1) =  real(sss1)  ;
     end
 end
 %% Save the susceptibilities
 x1x = squeeze(rechi1x); 
 x2x = squeeze(imchix);
-save(strcat('x1x_x2x_',num2str(Temperatures(:)*1000),'mK'),'fields','freq_total','x1x','x2x')
+save(strcat('x1x_x2x_',num2str(ttt*1000),'mK'),'fields','freq_total','x1x','x2x')
 
 x1y = squeeze(rechi1y); 
 x2y = squeeze(imchiy);
-save(strcat('x1y_x2y_',num2str(Temperatures(:)*1000),'mK'),'fields','freq_total','x1y','x2y')
+save(strcat('x1y_x2y_',num2str(ttt*1000),'mK'),'fields','freq_total','x1y','x2y')
 
 x1z = squeeze(rechi1z); 
 x2z = squeeze(imchiz);
-save(strcat('x1z_x2z_',num2str(Temperatures(:)*1000),'mK'),'fields','freq_total','x1z','x2z')
+save(strcat('x1z_x2z_',num2str(ttt*1000),'mK'),'fields','freq_total','x1z','x2z')
 %% Color plot the susceptibilities
 % % Color plot of the imaginary part of the susceptibility of x component
 %     hfig0 = figure (1);
