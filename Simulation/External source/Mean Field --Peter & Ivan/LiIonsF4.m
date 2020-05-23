cb24840fbf21e9bfe4ee2c11a018cfbd00169b86
skymemory88 <skymemory88@hotmail.com>
2020-05-23 14:28:13 +0200

    fixing accidental delete

---
+++ Simulation/External source/Mean Field --Peter & Ivan/LiIonsF4.m
@@ -0,0 +1,188 @@
+function [ion,history,E,V]=LiIonsF4(ion,temp,field,demagn,alpha)
+%Compute the magnetization and the alternated magnetization for a range
+%of temperatures and fields. (1-x) is the dilution. temp and h are the 
+%temperature and field arrays. xHypIso is the proportion of nuclear moments
+%isotops for Er. alpha is, in the case the sample is an ellipsiod (a,c,c),
+%the ratio a/c (for demagnetization tensor).
+
+global strategies;
+global rundipole
+
+t=tic;
+    
+% Matrix used to calculate order parameters from (alternating) moments
+alt=[1  1  1
+    -1  1  1
+    -1 -1  1
+     1 -1  1];  
+
+ion_mom_init = ion.mom;
+ 
+ion.Jmom=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
+ion.Jmom_norm=zeros([length(temp),size(field,2),size(ion.name,1)]);
+ion.Jmom_hyp=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
+
+ion.altJmom=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
+ion.altJmom_hyp=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
+
+ion.Js=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
+ion.Js_hyp=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
+
+ion.altJs=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
+ion.altJs_hyp=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
+
+history=cell([length(temp),size(field,2)]);
+ 
+%%
+% if length(temp) > size(field,2)
+% for i = 1:size(field,2)
+%     
+%     if length(temp) > 1 %Ions' initial moments ion.mom(:,:,1)=[1 0 0; 1
+% 0 0; -1 0 0; -1 0 0];      %Er % ion.mom(:,:,1)=[0 1 0; 0 1 0; 0 -1 0; 0
+% -1 0];      %Er ion.mom(:,:,2)=[0 0 1;  0 0 1;  0 0 1; 0 0 1]*2.6;  %Ho %
+% ion.mom(:,:,2)=[3.473 -0.045 0.1;  3.473 -0.045 0.1;  3.473 -0.045 0.1;
+% 3.473 -0.045 0.1];  %Ho ion.mom(:,:,3)=[1 0 0; -1 0 0; -1 0 0; 1 0 0];
+% Yb ion.mom(:,:,4)=[1 0 0; -1 0 0; -1 0 0; 1 0 0];      %Tm
+% ion.mom(:,:,5)=[1 0 0; -1 0 0; -1 0 0; 1 0 0];      %Gd ion.mom(:,:,6)=[0
+% 0 0; 0 0 0; 0 0 0; 0 0 0];      %Y ion.mom_hyp=ion.mom;
+%         rundipole = true;
+%     end
+% 
+%     for j = 1:length(temp)
+%         [ion,evolution,energy,v]=remf(ion,field(:,i)',temp(j),demagn,alpha);
+%         E(i,j,:)=energy; V(i,j,:,:)=v;
+% 
+% 
+%         for k=1:size(ion.name,1)
+%             ion.altJmom(:,j,i,k)=mean(alt.*ion.mom(:,:,k));
+%             ion.altJmom_hyp(:,j,i,k)=mean(alt.*ion.mom_hyp(:,:,k));
+%             ion.Jmom(:,j,i,k)=mean(ion.mom(:,:,k));
+%             ion.Jmom_norm(j,i,k)=norm(ion.mom(1,:,k));
+%             ion.Jmom_hyp(:,j,i,k)=mean(ion.mom_hyp(:,:,k));
+%             ion.Js(:,:,j,i,k)=ion.mom(:,:,k);
+%             ion.altJs(:,:,j,i,k)=alt.*ion.mom(:,:,k);
+%             ion.altJs_hyp(:,:,j,i,k)=alt.*ion.mom_hyp(:,:,k);
+%             
+%             history{j,i}=evolution;
+%         end
+%         
+%         if strategies.powerlaw && j>3 && j<length(temp) % Guess starting
+%         values for next temperature using powerlaw
+%             beta=0.5; % assume exponent 1/2. Could generalize to use more
+%             points and flexible exponent ibeta=1/beta;
+%             
+%             J1=zeros([4,3,size(ion.name,1)]);
+%             J2=zeros([4,3,size(ion.name,1)]);
+%             Tc=zeros([4,3,size(ion.name,1)]);
+%             Aa=zeros([4,3,size(ion.name,1)]);
+%             Jnew=zeros([4,3,size(ion.name,1)]); for k=1:size(ion.name,1)
+%                 J1(:,:,k)=squeeze(history{j,i}.(ion.name{k})(end,:,:));
+%                 J2(:,:,k)=squeeze(history{j-1,i}.(ion.name{k})(end,:,:));
+%                 Tc(:,:,k)=((J1(:,:,k)./J2(:,:,k)).^ibeta*temp(j-1)-temp(j))./((J1(:,:,k)./J2(:,:,k)).^ibeta-1);
+%                 can generalize to use more points if
+%                 isempty(find(((Tc(:,:,k)<=0) + (Tc(:,:,k) >
+%                 temp(end))),1)) % only do this if Tc guess is reasonable
+%                     Aa(:,:,k)=J2(:,:,k).^ibeta./(Tc(:,:,k)-temp(j-1)); if
+%                     isempty(find(~isfinite(Aa(:,:,k))+isnan(Aa(:,:,k)),1))
+%                     problem if Tc happens to be equal temp(j-1)
+%                         Jnew(:,:,k)=real((Aa(:,:,k).*(Tc(:,:,k)-temp(j+1))).^beta).*(temp(j+1)<Tc(:,:,k));
+%                         ion.mom(:,:,k)=Jnew(:,:,k);
+%                     end
+%                 end
+%             end
+%         end
+%     end
+%         cd('C:\Users\ikovacev\Documents\Projects\LiHoF4_EPR_MF_calculations_and_literature\saved_mf_results\3.42GHz\absorption')
+%         save('fieldscan22.mat','temp','field','ion','history','E','V','-v7.3')
+%         cd('C:\Users\ikovacev\Documents\Projects\LiHoF4_EPR_MF_calculations_and_literature')
+% end
+% 
+% else % length(temp) < size(field,2)
+%%
+for j = 1:length(temp)
+    % Reinitiate the angular moments at each temperature step (2020.01.02)
+    if size(field,2) > 1
+        ion.mom = ion_mom_init; %Reinitialize the spin moments at each field
+        ion.mom_hyp = ion.mom;
+        rundipole = true;
+    end
+
+    for i = 1:size(field,2)
+        [ion,evolution,energy,v,h_mf1]=remf(ion,field(:,i)',temp(j),demagn,alpha);
+        E(i,j,:)=energy;
+        V(i,j,:,:)=v;
+        h_mf2(i,:)=h_mf1(2,:);
+
+        for k=1:size(ion.name,1)
+            ion.altJmom(:,j,i,k)=mean(alt.*ion.mom(:,:,k));
+            ion.altJmom_hyp(:,j,i,k)=mean(alt.*ion.mom_hyp(:,:,k));
+            ion.Jmom(:,j,i,k)=mean(ion.mom(:,:,k));
+            ion.Jmom_hyp(:,j,i,k)=mean(ion.mom_hyp(:,:,k)); 
+%             ion.Jmom_norm(:,j,i,k)=norm(ion.mom(1,:,k)); % from originial code--Yikai (11.02.2020)
+            ion.Jmom_norm(j,i,k)=norm(ion.Jmom(:,j,i,k));
+            ion.Jmom_hyp_norm(j,i,k)=norm(ion.Jmom_hyp(:,j,i,k)); % addded on 11.02.2020 --Yikai
+            ion.Js(:,:,j,i,k)=ion.mom(:,:,k);
+            ion.Js_hyp(:,:,j,i,k)=ion.mom_hyp(:,:,k);
+            ion.altJs(:,:,j,i,k)=alt.*ion.mom(:,:,k);
+            ion.altJs_hyp(:,:,j,i,k)=alt.*ion.mom_hyp(:,:,k);
+            
+            history{j,i}=evolution;
+        end
+        
+        if strategies.powerlaw && j>3 && j<length(temp) % Guess starting values for next temperature using powerlaw
+            beta=0.5; % assume exponent 1/2. Could generalize to use more points and flexible exponent
+            ibeta=1/beta;
+            
+            J1=zeros([4,3,size(ion.name,1)]);
+            J2=zeros([4,3,size(ion.name,1)]);
+            Tc=zeros([4,3,size(ion.name,1)]);
+            Aa=zeros([4,3,size(ion.name,1)]);
+            Jnew=zeros([4,3,size(ion.name,1)]);
+            for k=1:size(ion.name,1)
+                J1(:,:,k)=squeeze(history{j,i}.(ion.name{k})(end,:,:));
+                J2(:,:,k)=squeeze(history{j-1,i}.(ion.name{k})(end,:,:));
+                Tc(:,:,k)=((J1(:,:,k)./J2(:,:,k)).^ibeta*temp(j-1)-temp(j))./((J1(:,:,k)./J2(:,:,k)).^ibeta-1); % can generalize to use more points
+                if isempty(find(((Tc(:,:,k)<=0) + (Tc(:,:,k) > temp(end))),1)) % only do this if Tc guess is reasonable
+                    Aa(:,:,k)=J2(:,:,k).^ibeta./(Tc(:,:,k)-temp(j-1));
+                    if isempty(find(~isfinite(Aa(:,:,k))+isnan(Aa(:,:,k)),1)) % problem if Tc happens to be equal temp(j-1)
+                        Jnew(:,:,k)=real((Aa(:,:,k).*(Tc(:,:,k)-temp(j+1))).^beta).*(temp(j+1)<Tc(:,:,k));
+                        ion.mom(:,:,k)=Jnew(:,:,k);
+                    end
+                end
+            end
+        end
+        eee(i,1,:)=energy;
+        vvv(i,1,:,:)=v;
+    end
+    fff = field;
+    ttt = temp(j);
+% Save the data split by temperatures when there are multi-dimensional, otherwise save data outside this function
+    if size(field,2) >1 && length(temp) >1
+        cd('G:\My Drive\File sharing\Programming scripts\Matlab\Simulation\External source\Mean Field --Peter & Ivan\output')
+        tit=['Li',ion.name(1:end), 'F4_', num2str(temp(j),'%3.3f'),'.mat'];
+        save(tit,'ttt','fff','eee','vvv','h_mf2','-v7.3')
+        cd('G:\My Drive\File sharing\Programming scripts\Matlab\Simulation\External source\Mean Field --Peter & Ivan')
+    end
+end
+
+% end
+
+for k=1:size(ion.name,1)    
+    ion.Jmom(:,:,:,k)=squeeze(ion.Jmom(:,:,:,k));
+    ion.Jmom_hyp(:,:,:,k)=squeeze(ion.Jmom_hyp(:,:,:,k));
+    
+    ion.Jmom_norm(:,:,k)=squeeze(ion.Jmom_norm(:,:,k));
+    ion.Jmom_hyp_norm(:,:,k)=squeeze(ion.Jmom_hyp_norm(:,:,k));
+    
+    ion.altJmom(:,:,:,k)=squeeze(ion.altJmom(:,:,:,k));
+    ion.altJmom_hyp(:,:,:,k)=squeeze(ion.altJmom_hyp(:,:,:,k));
+    
+    ion.Js(:,:,:,:,k)=squeeze(ion.Js(:,:,:,:,k));
+    ion.Js_hyp(:,:,:,:,k)=squeeze(ion.Js_hyp(:,:,:,:,k));
+    
+    ion.altJs(:,:,:,:,k)=squeeze(ion.altJs(:,:,:,:,k));
+    ion.altJs_hyp(:,:,:,:,k)=squeeze(ion.altJs_hyp(:,:,:,:,k));
+end
+
+
+toc(t)
---
+++ Simulation/External source/Mean Field --Peter & Ivan/output/MF_linear_response.m
@@ -0,0 +1,183 @@
+function MF_linear_response
+cd('G:\My Drive\File sharing\Programming scripts\Matlab\Simulation\External source\Mean Field --Peter & Ivan\output')
+clearvars;
+Options.plotting = false; % Decide whether or not to plot the data at the end
+Options.saving = true;
+
+% Temperatures = [0.250, 0.300, 0.350, 0.500];
+Temperatures = [0.100];
+    for ii = 1:length(Temperatures)
+        filenames = strcat('LHF_', num2str(Temperatures(ii),'%.3f'),'.mat');
+        load(filenames,'-mat','eee','fff','ttt','vvv'); % loads variables "fields", "temp", "E" and "V" 
+        % which are eigenstates and eigenvalues calculated in the mean-field model 
+        % as a function of transverse field and temperature
+        
+%         [rechi1x, imchix, rechi1y, imchiy, rechi1z, imchiz]=linear_response(eee,fff,ttt,vvv);
+        [~, ~, ~, ~, rechi1z, imchiz]=linear_response(eee,fff,ttt,vvv);
+    end
+%% Color plot the susceptibilities
+if Options.plotting == true
+% Color plot of the imaginary part of the susceptibility of x component
+    hfig0 = figure (1);
+    clf
+    hp1 = pcolor(fields(1,:),freq_total,squeeze(log(imchix)));
+    set(hp1, 'edgeColor','none')
+    caxis([-23 2]);
+    colorbar
+    xlabel('Magnetic field (T)')
+    ylabel('Frequency (GHz)')
+    title({'Imaginary part of Susceptibility (log scale)', 'in x direction'})
+    
+% Color plot of the imaginary part of the susceptibility of y component
+    hfig1 = figure (2);
+    clf
+    hp1 = pcolor(fields(1,:),freq_total,squeeze(log(imchiy)));
+    set(hp1, 'edgeColor','none')
+    caxis([-23 2]);
+    colorbar
+    xlabel('Magnetic field (T)')
+    ylabel('Frequency (GHz)')
+    title({'Imaginary part of Susceptibility (log scale)', 'in y direction'})
+    
+% Color plot the imaginary part of the susceptibilities of z component
+    hfig2 = figure (3);
+    clf
+    hp2 = pcolor(fields(1,:),freq_total,squeeze(log(imchiz)));
+    set(hp2, 'edgeColor','none')
+    caxis([-23 2]);
+    colorbar
+    xlabel('Magnetic field (T)')
+    ylabel('Frequency (GHz)')
+    title({'Imaginary part of Susceptibility (log scale)', 'in z direction'})
+
+% Plot the real part of the susceptibility of the z component
+    hfig4 = figure (4);
+    clf
+    set(hfig1,'position',[50 100 600 400])
+    hp2 = pcolor(fields(1,:),freq_total,squeeze(rechi1z));
+    set(hp2, 'edgeColor','none')
+    caxis([-23 2]);
+    colorbar
+    xlabel('Magnetic field (T)')
+    ylabel('Frequency (GHz)')
+    title({'Real part of Susceptibility', 'in z direction'})
+end
+%% Save the susceptibilities
+if Options.saving == true
+    % x1x = squeeze(rechi1x); 
+    % x2x = squeeze(imchix);
+    % save(strcat('x1x_x2x_',num2str(ttt*1000),'mK'),'fields','freq_total','x1x','x2x')
+    % 
+    % x1y = squeeze(rechi1y); 
+    % x2y = squeeze(imchiy);
+    % save(strcat('x1y_x2y_',num2str(ttt*1000),'mK'),'fields','freq_total','x1y','x2y')
+
+    x1z = squeeze(rechi1z); 
+    x2z = squeeze(imchiz);
+    save(strcat('x1z_x2z_',num2str(ttt*1000),'mK_5E-5meV_45deg'),'fields','freq_total','x1z','x2z')
+end
+end
+
+function [rechi1x, imchix, rechi1y, imchiy, rechi1z, imchiz]=linear_response(eee,fff,ttt,vvv)
+% Calculation of susceptibilities
+E = eee;Q
+V = vvv;
+fields = vecnorm(fff);
+freq_total = (1:0.02:5);
+
+imchix = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+imchiy = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+imchiz = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+
+rechi1x = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+rechi1y = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+rechi1z = double.empty(length(freq_total(1,:)),length(fields(1,:)),0);
+
+for m = 1:length(freq_total(1,:)) %calculate susceptibility for all frequencies
+    freq = freq_total (m);
+    J=8;
+    I=3.5;
+    gLande_Ho=1.25;
+
+    %Initiate J operators
+    Jz=diag(J:-1:-J); % Jz = -J, -J+1,...,J-1,J
+    Jzh=kron(Jz,eye(2*I+1)); % Expand Jz space to include nuclear degree of freedom
+    Jp=diag(sqrt((J-((J-1):-1:-J) ).*(J+1+( (J-1):-1:-J) )),1); % electronic spin ladder operator
+    Jm=Jp'; % electronic spin ladder operator
+    Jph=kron(Jp,eye(2*I+1)); % Expand Hilbert space
+    Jmh=kron(Jm,eye(2*I+1)); % Expand Hilbert space
+    Jxh=(Jph+Jmh)/2;
+    Jyh=(Jph-Jmh)/2i;
+    %tensor product of cristal field to include nuclear moments
+    %Initiate I operators
+    Iz=diag(I:-1:-I); %Iz = -I, -I+1,...,I-1,I
+    Izh=kron(eye(2*J+1),Iz); % Expand Hilbert space
+    Ip=diag(sqrt((I-[(I-1):-1:-I]).*(I+1+[(I-1):-1:-I])),1); % Nuclear spin ladder operator
+    Im=Ip'; % Nuclear spin ladder operator
+    Iph=kron(eye(2*J+1),Ip); % Expand to match the Hilbert space
+    Imh=kron(eye(2*J+1),Im); % Expand to match the Hilbert space
+    Ixh=(Iph+Imh)/2;
+    Iyh=(Iph-Imh)/2i;
+
+    f2E = 1/241.8;  % GHz to meV
+    omega = freq*f2E;   % define frequency sweep range (meV)
+%     gama = 0.00005; % define lifetime (meV) (theoretical basis?)
+    gama = 0.00005;
+
+    parfor k = 1:length(fields(1,:)) % calculate susceptibility for all fields
+        v = squeeze ( squeeze(V(k,:,:,:)) ); % Obtain the corresponding eigen vectors
+        en = squeeze ( squeeze(E(k,:,:)) ); % Obtain the corresponding eigen energies in meV
+%       N = length(en);
+%       chi_t = zeros(1,N^2);
+%       ll = 1;
+        beta = 1/(ttt*8.617E-2); %[meV^-1]
+        z=sum(exp(-beta*en));
+        zz=exp(-beta*en)/z;
+        [n,np]=meshgrid(zz,zz);
+        NN=n-np;
+        [ee,eep]=meshgrid(en,en);
+        EE = eep-ee-omega;
+        gamma = ones(size(EE))*gama;
+        G = gamma ./ (EE.^2 + gamma.^2); 
+        G1 = EE ./ (EE.^2 + gamma.^2);  
+
+        ELEf = gLande_Ho * 0.05788;     % Lande factor x Bohr magneton (meV T^-1)
+        NUCf = 4.732 * 3.1519e-5;   % Nuclear Lande factor
+        JxhT = Jxh * ELEf;
+        IxhT = Ixh * NUCf;
+
+        JyhT = Jyh * ELEf;
+        IyhT = Iyh * NUCf;
+
+        JzhT = Jzh * ELEf;
+        IzhT = Izh * NUCf;
+
+% Calculate susceptibilities along x-axis
+        ttx  = v'  * (JxhT+IxhT) * v; 
+        chi_tx  = (ttx) .* (ttx.') .* NN .* G;
+        chi_t1x = (ttx) .* (ttx.') .* NN .* G1;
+        sss=sum(sum(chi_tx)); 
+        sss1=sum(sum(chi_t1x));
+        imchix  (m,k,1) =  real(sss)   ;
+        rechi1x (m,k,1) =  real(sss1)  ; 
+
+% Calculate susceptibilities along y-axis
+        tty  = v'  * (JyhT+IyhT) * v; 
+        chi_ty  = (tty) .* (tty.') .* NN .* G;
+        chi_t1y = (tty) .* (tty.') .* NN .* G1;
+        sss=sum(sum(chi_ty));
+        sss1=sum(sum(chi_t1y));
+        imchiy  (m,k,1) =  real(sss)   ;
+        rechi1y (m,k,1) =  real(sss1)  ;   
+
+% Calculate susceptibilities along c-axis
+        ttz  = v'  * (JzhT+IzhT) * v;
+        chi_tz  = (ttz) .* (ttz.') .* NN .* G;
+        chi_t1z = (ttz) .* (ttz.') .* NN .* G1;
+        sss=sum(sum(chi_tz));
+        sss1=sum(sum(chi_t1z));
+        imchiz  (m,k,1) =  real(sss)   ;
+        rechi1z (m,k,1) =  real(sss1)  ;
+    end
+end
+end
