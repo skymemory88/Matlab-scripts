134b788880af42d2177823f54b376fbc766514ea
Yang Yikai <yiyang@intranet.epfl.ch>
2020-06-26 16:25:01 +0200

    unfollow

--- Simulation/External source/Monte Carlo/Parallel-CMC code/energy_update.m
+++
@@ -1,5 +0,0 @@
-function dE=energy_update(ion,field,lattice,system_size,inter,new_ion,new_mom, new_E_Zee, old_E_Zee)
-% update to the energy
-% ones has to make sure that the effective moment has been computed and
-% saved in lattice{i} for each site and that new_mom is the effective new
-% moment, old_E_Zee and new_E_Zee are the energies computed by cl_eff_mod
