7bf800735994a637f92d40057f9608c35db9ea21
skymemory88@hotmail.com <skymemory88@hotmail.com>
2021-02-21 15:22:31 +0100

    update

--- Plot functions/Fieldscan/iptopt_0.m
+++ Plot functions/Fieldscan/iptopt_0.m
@@ -1,10 +1,11 @@
-function [fitresult, gof] = iptopt_0(x, y, field, P0, low, upr, weight)
+function [fitresult, gof] = iptopt_0(x, y, field, B0, spin, P0, low, upr, weight, plt)
 %CREATEFIT(omega,S11)
 %  S11 input-output fit:
 %      X Input : frequency
-%      Field : current magnetic field
-%      P0: Initial guesses for fitting parameters (Ke,w0,G,Br,gamma)
+%      alpha : coefficient of linearized w0(B)
+%      P0: Initial guesses for fitting parameters (Ke,Ki,w0,Gc,gamma)
 %      Y Output: |S11|
+%      Plt (true of false): Plot the fit along with data
 %  Output:
 %      fitresult : a fit object representing the S11 parameter.
 %      gof : structure with goodness-of fit info.
@@ -12,25 +13,27 @@ function [fitresult, gof] = iptopt_0(x, y, field, P0, low, upr, weight)
 %  See also FIT, CFIT, SFIT.
 %% Fit: 'S11_inptopt_fit'.
 [xData, yData] = prepareCurveData( x, y );
-
 % Set up fittype and options.
-ft = fittype( @(kpe, w0, Gc, Br, gma, field, x) mag2db(abs(1+kpe./(1i*(x-w0) - kpe + Gc/(1i*(field-Br)+gma)))),...
-    'independent', {'x'}, 'dependent', {'y'}, 'coefficients',{'kpe', 'w0', 'Gc', 'Br', 'gma'}, 'problem', {'field'});
+ft = fittype( @(kpe, kpi, w0, Gc, gma, shift, field, B0, spin, x) (shift + abs(1+kpe./...
+    (1i*(x-w0) - (kpe + kpi) + Gc^2./(1i*(x-(spin*(field-B0)+w0) )-gma)))),...
+    'independent', {'x'}, 'dependent', {'y'}, 'coefficients',{'kpe', 'kpi', 'w0', 'Gc', 'gma', 'shift'}, 'problem', {'field','B0', 'spin'});
 opts = fitoptions( 'Method', 'NonlinearLeastSquares');
 opts.Display = 'Off';
-opts.StartPoint = [P0(1) P0(2) P0(3) P0(4) P0(5)];
-opts.Lower = [low(1) low(2) low(3) low(4) low(5)];
-opts.Upper = [upr(1) upr(2) upr(3) upr(4) upr(5)];
+opts.StartPoint = [P0(1) P0(2) P0(3) P0(4) P0(5) P0(6)];
+opts.Lower = [low(1) low(2) low(3) low(4) low(5) low(6)];
+opts.Upper = [upr(1) upr(2) upr(3) upr(4) upr(5) upr(6)];
 opts.Weights = weight;
 
 % Fit model to data.
-[fitresult, gof] = fit( xData, yData, ft, 'problem', field, opts);
+[fitresult, gof] = fit( xData, yData, ft, 'problem', {field, B0, spin}, opts);
 
-% Plot fit with data.
-figure( 'Name', 'S11_inptopt_fit' );
-h = plot( fitresult, xData, yData );
-legend( h, 'y vs. x', 'S11_inptopt_fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
-% Label axes
-xlabel( 'x', 'Interpreter', 'none' );
-ylabel( 'y', 'Interpreter', 'none' );
-grid on
+if plt
+    % Plot fit with data.
+    figure( 'Name', sprintf('S11_inptopt_fit at B = %.2f T',field) );
+    h = plot( fitresult, xData, yData );
+    legend( h, 'Data', 'S11_fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
+    % Label axes
+    xlabel( 'Frequency (GHz)', 'Interpreter', 'none' );
+    ylabel( '|S11|', 'Interpreter', 'none' );
+    grid on
+end
