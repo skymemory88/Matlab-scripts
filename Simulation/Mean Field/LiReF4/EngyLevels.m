dc742dd42d68a90b6263d494564ee343875dd8cc
skymemory88@hotmail.com <skymemroy88@hotmail.com>
2021-06-24 19:44:45 +0200

    add <J> plots

--- Simulation/Mean Field/LiReF4/EngyLevels.m
+++ Simulation/Mean Field/LiReF4/EngyLevels.m
@@ -3,24 +3,27 @@ function EngyLevels
 % hold on
 clearvars
 % temp =  [1.774 1.776 1.778 1.780 1.781 1.782 1.783 1.784 1.785 1.787];
-temp = 0.13;
-N_level = 7; % Number of levels to plot
+temp = 0.15;
+theta = [0.0]; % Angle (in degrees) between magnetic field and c-axis in a-c plane
+phi = [0]; % Angle (in degrees) in a-b plane
+N_level = 7; % Number of transition levels to plot
 
+Options.ion = 'Ho'; % support 'Er' and 'Ho'
+Options.hyp = 1.00; % Hyperfine isotope proportion
 Options.RPA = false; % not enabled yet
 Options.Elevel = false;
 Options.Ediff = true;
 Options.Espan = false;
+Options.Js = false;
 Options.savedata = false;
 Options.savegif = false;
 Options.location =... % Location to load/save files
-    'G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Simulations results\Matlab\Susceptibilities\without Hz_I';           
+    ['G:\My Drive\File sharing\PhD program\Research projects\Li',Options.ion,...
+        'F4 project\Data\Simulations\Matlab\Susceptibilities\with Hz_I'];
 
-color = ["black","red","blue","magenta","green","yellow","cyan"];
-theta = [0.0]; % Angle (in degrees) between magnetic field and c-axis in a-c plane
-phi = [30.0]; % Angle (in degrees) in a-b plane
 marker = [":","-.","--","-"];
-% theta = 0;
-% color = {[255 127 0], [255 0 127], [127 0 255], [0 127 255], [0 255 127]} ;
+color = ["black","red","blue","magenta","green","yellow","cyan"];
+% color = {[255 127 0], [255 0 127], [127 0 255], [0 127 255], [0 255 127]};
 lg = strings(1,numel(theta),numel(phi)); % Create an empty array for legends
 figs_E = gobjects(N_level+1,numel(theta),numel(phi));
 figs_dE = gobjects(N_level,numel(theta),numel(phi));
@@ -38,13 +41,14 @@ for iter = 1:numel(temp)
     for iter2 = 1:numel(theta)
         for iter3 = 1:numel(phi)
             if Options.RPA == true
-                lname=['Hscan_LiHoF4_', sprintf('%1$3.3fK_%2$.1fDeg_%3$.1fDeg',temp(iter),theta(iter2),phi(iter3)),'.mat'];
+                lname=['Hscan_Li',Options.ion,'F4_',...
+                    sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_hp=%4$.2f',temp(iter),theta(iter2),phi(iter3),Options.hyp),'.mat'];
             else
-                lname=['Hscan_LiHoF4_', sprintf('%1$3.3fK_%2$.1fDeg_%3$.1fDeg',temp(iter),theta(iter2),phi(iter3)),'.mat'];
+                lname=['Hscan_Li',Options.ion,'F4_',...
+                    sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_hp=%4$.2f',temp(iter),theta(iter2),phi(iter3),Options.hyp),'.mat'];
             end
             file = fullfile(Options.location,lname);
-            %         load(lname,'-mat','eee','fff');
-            load(file,'-mat','eee','fff');
+            load(file,'-mat','vvv','ttt','eee','fff','ion');
             
             fields = vecnorm(fff);
             E2f = 241.8; % Convert Energy to frequency
@@ -77,8 +81,8 @@ for iter = 1:numel(temp)
                 figs_E(:, iter2, iter3) = plot(fields,E(:,1:N_level+1),'Color',[35 107 142]/255,'linewidth',2);
                 hold on
                 set(gca,'fontsize',15);
-                % text(8,(j-1)*D+0.02,'0.1');
-                %         set(fig1,'position',[100 100 600 300])
+%                text(8,(j-1)*D+0.02,'0.1');
+%                set(fig1,'position',[100 100 600 300])
                 xlabel('Field (T)','FontSize',15)
                 ylabel('Energy (GHz)','FontSize',15)
                 grid off
@@ -96,13 +100,14 @@ for iter = 1:numel(temp)
             %% Energy difference bewteen neighbour levels
             if Options.Ediff == true
                 for i=1:N_level
-                    Ediff(i,:)=E(:,i+1)-E(:,i);
+                    Ediff(i,:) = E(:,i+1)-E(:,i);
                 end
-                %         % Plot the lowest energy difference between the 8 levels
+%               % Plot the lowest energy difference between the 8 levels
                 % frequency = { '1.682 GHz', '3.436 GHz', '3.924 GHz', '4.449 GHz', '5.604 GHz' } ;
-%                 fig_dE = figure;
-%                 figs_dE(:, iter2, iter3) = plot(fields, Ediff, 'Marker', 'none', 'LineStyle',marker(1), 'Color',color(iter3),'LineWidth',2);
-                figs_dE(:, iter2, iter3) = plot(fields, Ediff, 'Marker', 'none', 'LineStyle',marker(1), 'Color','black','LineWidth',1.5);
+%                 figure;
+%                 figs_dE(:, iter2, iter3) = plot(fields, Ediff, 'Marker', 'none', 'LineStyle', marker(1), 'Color', color(iter3),'LineWidth',2);
+                figs_dE(:, iter2, iter3) = plot(fields, Ediff, 'Marker', 'none', 'LineStyle', marker(1), 'Color','r','LineWidth',1.5);
+%                 figs_dE(:, iter2, iter3) = plot(fields, Ediff, 'Marker', 'none', 'LineStyle', marker(1),'LineWidth',1.5);
                 hold on
 %                 hpcfr1 = plot(fields, Ediff,'Color','blue','linewidth',2);
 %                 hpcfr1 = plot(fields, Ediff(2:end,:),'Color','black','linewidth',2);
@@ -112,32 +117,31 @@ for iter = 1:numel(temp)
 %                 % Plot the resonant frequency of a bare cavity
 %                 f_cav = 3.54; % Set resonant frequency of the bare cavity
 %                 plot([0 max(fields)],[f_cav f_cav],'-r','LineWidth',1.5);
-%                 
-%                 % Load experimental data of line crossings and superimpose the plot onto the calculated data
-%                 load('f0_BC.mat');
-%                 plot(B(:,1),B(:,2),'ro','MarkerFaceColor','red');
-%                 
+                
 %                 yyaxis(gca,'left');
                 set(gca,'fontsize',15,'Xtick',0:1:max(fields));
                 set(gca,'XTickLabelRotation',0)
 %                 xticks(linspace(0.1,max(fields),10))
 %                 text(8,(j-1)*D+0.02,'0.1');
 %                 set(fig4,'position',[800 100 600 300]);
-                xlabel('Field (T)','FontSize',15);
+                xlabel('Magnetic Field (T)','FontSize',15);
                 ylabel('Energy gap (GHz)','FontSize',15);
                 grid off
                 box on;
                 xlim([min(fields) max(fields)]);
+                xticks('auto')
                 ylim([0.9*min(Ediff,[],'All') 1.1*max(Ediff,[],'All')]);
 %                 ylim([1 5]);
 %                 ylim([3.59 3.71]) % adjust y-axis range to fit experimental data
-                tit4='Energy gaps at ';
+                tit4 = 'Energy gaps at ';
                 title([tit4 sprintf('T = %.3f K',temp(iter))],'FontSize',15)
                 if Options.savegif == true
                     drawnow
                     frame = getframe(fig_dE);
                     Ediff_frame{im_idx} = frame2im(frame);
                 end
+                lgd = legend(squeeze(figs_dE(1,:,:)),lg);
+                lgd.FontSize = 12;
             end
             %% Frequency span of the transitions at each field
             if Options.Espan == true
@@ -159,15 +163,58 @@ for iter = 1:numel(temp)
                 tit4='Frequency range the energy levels cover';
                 title(tit4,'FontSize',15)
             end
-            lg(iter, iter2, iter3) = [num2str(temp(iter)*1000,"T = %u mK, A = A_{th}"),num2str(theta(iter2),", theta = %.1f"),char(176),num2str(phi(iter3),", phi = %.1f"),char(176)];
+%             lg(iter, iter2, iter3) = [num2str(temp(iter)*1000,"T = %u mK, A = A_{th}"),', \theta', num2str(theta(iter2)," = %.1f"),...
+%                                       char(176),', \phi', num2str(phi(iter3)," = %.1f"),char(176)];
+            lg(iter, iter2, iter3) = ['\phi', num2str(phi(iter3)," = %.1f"), char(176)];
             if Options.savegif == true
                 im_idx = im_idx + 1;
+            end
+            %% Spin expectations
+            if Options.Js == true
+                Jx = cell(1,4);
+                Jy = cell(1,4);
+                Jz = cell(1,4);
+                Jnorm = cell(1,4);
+                
+                figure;
+                hold on
+                for ii = 1:4
+                    Jx{ii} = double.empty(4,length(fff(1,:)),0);
+                    Jy{ii} = double.empty(4,length(fff(1,:)),0);
+                    Jz{ii} = double.empty(4,length(fff(1,:)),0);
+                    for jj = 1:length(fff(1,:))
+                        Jx{ii}(:,jj,1) = ion.Js_hyp(ii,1,jj,1);
+                        Jy{ii}(:,jj,1) = ion.Js_hyp(ii,2,jj,1);
+                        Jz{ii}(:,jj,1) = ion.Js_hyp(ii,3,jj,1);
+                    end
+                    Jnorm{ii} = vecnorm([Jx{ii};Jy{ii};Jz{ii}]);
+                    plot(vecnorm(fff),Jnorm{ii},'o')
+                end
+                legend(strcat(Options.ion,'-1'),strcat(Options.ion,'-2'),strcat(Options.ion,'-3'),strcat(Options.ion,'-4'))
+                xlabel('Magnetic field (T)')
+                ylabel('<J>')
+                
+                Jmx = double.empty(4,length(fff(1,:)),0);
+                Jmy = double.empty(4,length(fff(1,:)),0);
+                Jmz = double.empty(4,length(fff(1,:)),0);
+                for ii = 1:length(fff(1,:))
+                    Jmx(:,ii,1) = ion.Jmom_hyp(1,ii,1);
+                    Jmy(:,ii,1) = ion.Jmom_hyp(2,ii,1);
+                    Jmz(:,ii,1) = ion.Jmom_hyp(3,ii,1);
+                end
+                figure
+                hold on
+                plot(vecnorm(fff),Jmx,'-s')
+                plot(vecnorm(fff),Jmy,'-s')
+                plot(vecnorm(fff),Jmz,'-s')
+                legend('<Jx>','<Jx>','<Jz>')
+                xlabel('Magnetic field (T)')
+                ylabel('<J_i>')
             end
         end
     end
 end
-% lgd = legend(figs(1,:,:),lg);
-% lgd.FontSize = 12;
+
 if Options.savedata == true
     filename = strcat('sim_',num2str(temp*1000,'%u'),'mK_trans.mat','-v7.3');
     save(filename,'fields','Ediff');
