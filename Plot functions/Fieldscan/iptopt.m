50359cadbfd90f047dc9dbe18ffc81c5cae1cede
skymemory88@hotmail.com <skymemroy88@hotmail.com>
2021-09-03 16:02:44 +0200

    udpate

--- Plot functions/Fieldscan/iptopt.m
+++ Plot functions/Fieldscan/iptopt.m
@@ -2,8 +2,7 @@ function [fitresult, gof] = iptopt(x, y, field, P0, low, upr, weight, plt)
 %CREATEFIT(omega,S11)
 %  S11 input-output fit:
 %      X Input : frequency
-%      alpha : coefficient of linearized w0(B)
-%      P0: Initial guesses for fitting parameters (Ke,Ki,w0,Gc,gamma)
+%      P0: Initial guesses for fitting parameters (Ke,Ki,omega,Gc,gamma,wc)
 %      Y Output: |S11|
 %      Plt (true of false): Plot the fit along with data
 %  Output:
@@ -14,14 +13,14 @@ function [fitresult, gof] = iptopt(x, y, field, P0, low, upr, weight, plt)
 %% Fit: 'S11_inptopt_fit'.
 [xData, yData] = prepareCurveData( x, y );
 % Set up fittype and options.
-ft = fittype( @(kpe, kpi, omega, Gc, gma, w0, x) (abs(1+2*kpe./...
-    (1i*(x-w0) - (kpe + kpi) + Gc^2./(1i*(x-omega )-gma)))),...
-    'independent', {'x'}, 'dependent', {'y'}, 'coefficients',{'kpe', 'kpi', 'omega', 'Gc', 'gma', 'w0'});
+ft = fittype( @(kpe, kpi, omega, Gc, gma, wc, attn, x) (abs(1+2*kpe./...
+    (1i*(x-wc) - (kpe + kpi) + Gc^2./(1i*(x-omega)-gma))).*attn),...
+    'independent', {'x'}, 'dependent', {'y'}, 'coefficients',{'kpe', 'kpi', 'omega', 'Gc', 'gma', 'wc','attn'});
 opts = fitoptions( 'Method', 'NonlinearLeastSquares');
 opts.Display = 'Off';
-opts.StartPoint = [P0(1) P0(2) P0(3) P0(4) P0(5) P0(6)];
-opts.Lower = [low(1) low(2) low(3) low(4) low(5) low(6)];
-opts.Upper = [upr(1) upr(2) upr(3) upr(4) upr(5) upr(6)];
+opts.StartPoint = [P0(1) P0(2) P0(3) P0(4) P0(5) P0(6)  P0(7)];
+opts.Lower = [low(1) low(2) low(3) low(4) low(5) low(6) low(7)];
+opts.Upper = [upr(1) upr(2) upr(3) upr(4) upr(5) upr(6) upr(7)];
 opts.Weights = weight;
 
 % Fit model to data.
