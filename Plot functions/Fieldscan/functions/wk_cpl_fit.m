148dad4e3809c59250a80aed274b2891ab88abd8
skymemory88@hotmail.com <skymemroy88@hotmail.com>
2021-09-15 11:48:51 +0200

    fitting parameter adjustment

--- Plot functions/Fieldscan/functions/wk_cpl_fit.m
+++ Plot functions/Fieldscan/functions/wk_cpl_fit.m
@@ -1,6 +1,6 @@
-function [fitresult, gof] = wk_cpl_fit(H0, f0, spin, hPara, fPara)
+function [fitresult, gof] = wk_cpl_fit(H0, f0, init, bdl, bdh, weight, plt)
 %CREATEFIT(H0,F0)
-%  Create a fit.
+%  Strong coupling fit of resonant frequency.
 %
 %  Data for 'wk_cpl_fit' fit:
 %      X Input : H0
@@ -16,23 +16,26 @@ function [fitresult, gof] = wk_cpl_fit(H0, f0, spin, hPara, fPara)
 [xData, yData] = prepareCurveData( H0, f0 );
 
 % Set up fittype and options.
-ft = fittype( 'wc - g^2*spin*(x-x0)/((spin*(x-x0))^2+gamma^2)', 'independent', 'x', 'dependent', 'y');
+ft = fittype( 'wc - g^2*slope*(x-x0)/((slope*(x-x0))^2 + gamma^2)', 'independent', 'x', 'dependent', 'y');
 opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 opts.Display = 'Off';
-opts.StartPoint = [0.05  1e2  spin  fPara(1)  hPara(1)]; % [wc g spin x0 gamma]
-opts.Lower = [0    0   -Inf  fPara(2)  hPara(2)];
-opts.Upper = [1    1    Inf  fPara(3)  hPara(3)];
+opts.StartPoint = [init(1) init(2)  init(3)  init(4)  init(5)]; % [g  gamma slope wc x0]
+opts.Lower = [bdl(1)  bdl(2)  bdl(3)  bdl(4)  bdl(5)];
+opts.Upper = [bdh(1)  bdh(2)  bdh(3)  bdh(4)  bdh(5)];
+opts.Weights = weight;
 
 % Fit model to data.
 [fitresult, gof] = fit( xData, yData, ft, opts );
 
-% Plot fit with data.
-figure( 'Name', 'Resonant frequency fit' );
-h = plot( fitresult, xData, yData );
-legend( h, 'f0 vs. H0', 'Weak coupling fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
-% Label axes
-xlabel( 'H0', 'Interpreter', 'none' );
-ylabel( 'f0', 'Interpreter', 'none' );
-grid on
+% Plot fit with data
+if plt
+    figure( 'Name', 'Resonant frequency fit' );
+    h = plot( fitresult, xData, yData );
+    legend( h, 'f0 vs. H0', 'Weak coupling fit', 'Location', 'NorthEast', 'Interpreter', 'none' );
+    % Label axes
+    xlabel( 'H0', 'Interpreter', 'none' );
+    ylabel( 'f0', 'Interpreter', 'none' );
+    grid off
+end
 
 
