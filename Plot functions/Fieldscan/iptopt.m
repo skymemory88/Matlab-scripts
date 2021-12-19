1e8e17fe8dfde54b0900d67ea12ee9d559b74e41
skymemory88@hotmail.com <skymemory88@hotmail.com>
2021-12-19 12:58:41 +0100

    apply coupling strength coefficient && off-resonance susceptibility component

--- Plot functions/Fieldscan/iptopt.m
+++ Plot functions/Fieldscan/iptopt.m
@@ -1,4 +1,4 @@
-function [fitresult, gof] = iptopt(x, y, field, P0, low, upr, weight, plt)
+function [fitresult, gof] = iptopt(x, y, coef, P0, low, upr, weight, plt)
 %CREATEFIT(omega,S11)
 %  S11 input-output fit:
 %      X Input : frequency
@@ -11,21 +11,36 @@ function [fitresult, gof] = iptopt(x, y, field, P0, low, upr, weight, plt)
 %      gof : structure with goodness-of fit info.
 %
 %  See also FIT, CFIT, SFIT.
-%% Fit: 'S11_inptopt_fit'.
+% Fit: 'S11_iptopt_fit'.
+mu0 = 4*pi*10^-7; % Vacuum permeability ([H/m])
+hbar = 1.055E-34; % Reduced Planck constant [J.s]
+meV2J = 1.602217e-22; % [J/meV]
+rho = 4e30/(5.17*5.17*10.75); % Holmium (magnetic moment) number density [m^-3]
+f2E = hbar*2*pi*10^9/meV2J; % [meV/GHz]
+
+kpe = coef(1); % external dissipation rate
+kpi = coef(2); % internal dissipation rate
+wc = coef(3); % cavity resonance frequency
+field = coef(4); % current magnetic field
+fill = coef(5); % Filling factor (Sample specific)
+
 [xData, yData] = prepareCurveData( x, y );
+g = sqrt(mu0*wc*10^9*2*pi*rho*fill/hbar/2); % susceptibility prefactor [T.(J.s)^-1]
+g = g * meV2J * f2E * 10^-9;
 % Set up fittype and options.
-ft = fittype( @(kpe, kpi, omega, Gc, gma, wc, attn, x) (abs(1 + 2*kpe./...
-    (1i*(x-wc) - (kpi + kpe) + Gc^2./(1i*(x-omega )-gma))).*attn),...
-    'independent', {'x'}, 'dependent', {'y'}, 'coefficients',{'kpe', 'kpi', 'omega', 'Gc', 'gma', 'wc','attn'});
+ft = fittype( @(xr_off, xi_off, omega, Gc, gma, attn, kpe, kpi, wc, x) (abs(1 + 2*kpe./...
+    (1i*(x - wc) - (kpi + kpe) + 1i*g^2*(xr_off + 1i*xi_off + Gc./(1i*(x-omega )-gma)))).*attn),...
+    'independent', {'x'}, 'dependent', {'y'}, 'coefficients',{'xr_off', 'xi_off', 'omega', 'Gc', 'gma','attn'},...
+    'problem',{'kpe','kpi','wc'});
 opts = fitoptions( 'Method', 'NonlinearLeastSquares');
 opts.Display = 'Off';
-opts.StartPoint = [P0(1) P0(2) P0(3) P0(4) P0(5) P0(6)  P0(7)];
-opts.Lower = [low(1) low(2) low(3) low(4) low(5) low(6) low(7)];
-opts.Upper = [upr(1) upr(2) upr(3) upr(4) upr(5) upr(6) upr(7)];
+opts.StartPoint = [P0(1) P0(2) P0(3) P0(4) P0(5) P0(6)];
+opts.Lower = [low(1) low(2) low(3) low(4) low(5) low(6)];
+opts.Upper = [upr(1) upr(2) upr(3) upr(4) upr(5) upr(6)];
 opts.Weights = weight;
 
 % Fit model to data.
-[fitresult, gof] = fit( xData, yData, ft, opts);
+[fitresult, gof] = fit( xData, yData, ft, opts, 'problem',{kpe, kpi, wc});
 
 if plt
     % Plot fit with data.
