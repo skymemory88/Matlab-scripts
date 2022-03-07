09d08726848b0b20975c33e6e59fbb0493e0b35f
skymemory88@hotmail.com <skymemory88@hotmail.com>
2022-03-07 12:40:16 +0100

    adding CF rotation

--- Simulation/Mean Field/LiReF4/cf.m
+++ Simulation/Mean Field/LiReF4/cf.m
@@ -1,6 +1,6 @@
 function [Hcf] = cf(J,B,rot)
 rot = rot*pi/180; % degree to rad
-rot_opt = 'operator'; % rotation by 'operator'/'coefficient'
+rot_opt = 'coefficient'; % rotation by 'operator'/'coefficient'
 %----- Form the J operators
 n = 2*J+1;
 Jz = diag(J:-1:-J);
@@ -18,13 +18,12 @@ O44c = (Jp^4+Jm^4)/2;
 O44s = (Jp^4-Jm^4)/(1i*2);
 
 O60 = 231*Jz^6 - (315*X-735)*Jz^4 + (105*X^2-525*X+294)*Jz^2 + (-5*X^3+40*X^2-60*X)*eye(n);
-O64c = ((11*Jz^2-X*eye(n)-38*eye(n)) * (Jp^4+Jm^4)+(Jp^4+Jm^4) * (11*Jz^2-X*eye(n)-38*eye(n)))/4;
-O64s = ((11*Jz^2-X*eye(n)-38*eye(n)) * (Jp^4-Jm^4)+(Jp^4-Jm^4) * (11*Jz^2-X*eye(n)-38*eye(n)))/(1i*4);  
+O64c = ((11*Jz^2-X*eye(n)-38*eye(n)) * (Jp^4+Jm^4) + (Jp^4+Jm^4) * (11*Jz^2-X*eye(n)-38*eye(n)))/4;
+O64s = ((11*Jz^2-X*eye(n)-38*eye(n)) * (Jp^4-Jm^4) + (Jp^4-Jm^4) * (11*Jz^2-X*eye(n)-38*eye(n)))/(1i*4);  
 
 %----- Crystal field rotation
 switch rot_opt
-    case 'operator'
-        % rotate crystal field (method 1)
+    case 'operator' % rotate crystal field (method 1)
         Ur = [cos(4*rot)  -sin(4*rot)
               sin(4*rot)  +cos(4*rot)];
         Ur = kron(Ur,eye(2*J+1));
@@ -44,15 +43,16 @@ switch rot_opt
         % O44s = sin(4*rot)*O44co + cos(4*rot)*O44so;
         % O64c = cos(4*rot)*O64co - sin(4*rot)*O64so;
         % O64s = sin(4*rot)*O64co + cos(4*rot)*O64so;
-    case 'coefficient'
-        % rotate crystal field (method 2)
+    case 'coefficient' % rotate crystal field (method 2)
         Br = [ cos(4*rot)  sin(4*rot)
               -sin(4*rot)  cos(4*rot)];
-        B44 = Br * ([B(3); B(4)]);
+        B44 = Br * ([B(3) B(4)]');
+        B44 = B44';
         B(3) = B44(1);
         B(4) = B44(2);
         
-        B64 = Br * ([B(6); B(7)]);
+        B64 = Br * ([B(6) B(7)]');
+        B64 = B64';
         B(6) = B64(1);
         B(7) = B64(2);
         
@@ -64,7 +64,7 @@ switch rot_opt
         % B(6) = cos(4*rot)*B64c + sin(4*rot)*B64s;
         % B(7) = -sin(4*rot)*B64c + cos(4*rot)*B64s;
     otherwise
+        % No rotation
 end
-
 Hcf = B(1)*O20 + B(2)*O40 + B(3)*O44c + B(4)*O44s + B(5)*O60 + B(6)*O64c + B(7)*O64s;
 end
--- Simulation/Mean Field/LiReF4/gLande.m
+++ Simulation/Mean Field/LiReF4/gLande.m
@@ -1,6 +1,7 @@
 function [gLande] = gLande(L,S)
 
-J=L+S;
+J = L + S;
 
-gLande=(3/2)+((S*(S+1)-L*(L+1))/(2*J*(J+1)));
+gLande = (3/2)+((S.*(S+1)-L.*(L+1))./(2.*J.*(J+1)));
+
 end
