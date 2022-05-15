9ed84604c5ee124978904640375f74725b2593bd
skymemory88@hotmail.com <skymemroy88@hotmail.com>
2022-05-15 02:04:46 +0200

    make the plot interactive and live updates

--- Simulation/Topological phases/Haldane_model.m
+++ Simulation/Topological phases/Haldane_model.m
@@ -1,30 +1,66 @@
 % Haldane model
-lattc = 1; % lattice constant
+t2min = 0;
+t2max = 0.5;
+mmin = 0;
+mmax = 2*t2max;
+lattc = 1.0; % lattice constant
+
+fObj = figure;
+ax = axes(fObj);
+box on
+hold(ax,'on');
+[KX, KY, Er] = kBands(lattc, 0, 0);
+uband = surf(ax, KX, KY, squeeze(Er(:,:,1)));
+uband.EdgeColor = 'none';
+lband = surf(ax,KX, KY, squeeze(Er(:,:,2)));
+lband.EdgeColor = 'none';
+xlabel(ax, '$\vec{k}_x$','Interpreter','latex')
+ylabel(ax, '$\vec{k}_y$','Interpreter','latex')
+zlabel(ax, 'Energy')
+set(gca,'FontSize',14)
+view(30,45)
+
+slider1 = uicontrol('Parent', fObj, 'Style', 'slider',...
+    'OuterPosition', [100 400 150 20], 'Value', 0, 'min', t2min, 'max', t2max);
+sld1.label = uicontrol('Parent',fObj,'Style','text','Position', [250 400 40 20],...
+                'String','Tnnn');
+            
+slider2 = uicontrol('Parent', fObj, 'Style', 'slider',...
+    'OuterPosition', [100 380 150 20], 'Value', 0, 'min', mmin, 'max', mmax);
+sld2.label = uicontrol('Parent',fObj,'Style','text', 'Position',[250 380 40 20],...
+                'String','Mass');
+            
+slider1.Callback = @(es1,ed) band_update([uband lband], lattc, slider2.Value, es1.Value);
+slider2.Callback = @(es2,ed) band_update([uband lband], lattc, es2.Value, slider1.Value);
+
+function band_update(figs, lattc, mass, tnnn)
+[~, ~, Er] = kBands(lattc, mass, tnnn);
+figs(1).ZData = Er(:,:,1);
+figs(2).ZData = Er(:,:,2);
+drawnow
+end
+
+function [KX, KY, Er] = kBands(lattc, mass, tnnn)
 tnn = 1.0; % nearest neighbour hopping amplitude
-tnnn = 0.0; % next nearest neighbour hopping amplitude
-mass = 0.2; % mass term
+
+% Pauli matrices
+sigx = [0   1;  1   0];
+sigy = [0 -1i;  1i  0];
+sigz = [1   0;  0  -1];
 
 % unit vector
 a = {[1 0].*lattc;
     [-1/2 -sqrt(3)/2].*lattc;
     [-1/2 sqrt(3)/2].*lattc};
 
-b = {[3/2 -sqrt(3)/2].*lattc;
+b = {[0 -sqrt(3)].*lattc;
      [3/2 +sqrt(3)/2].*lattc;
-     [-3/2 +sqrt(3)/2].*lattc;
-     [-3/2 -sqrt(3)/2].*lattc
-     [0 -sqrt(3)].*lattc
-     [0 +sqrt(3)].*lattc};
-
+     [-3/2 +sqrt(3)/2].*lattc};
+ 
 % k-vector
 kx = linspace(-pi/lattc, pi/lattc, 200);
 ky = linspace(-pi/lattc, pi/lattc, 200);
 [KX, KY] = meshgrid(kx,ky);
-
-% Pauli matrices
-sigx = [0 1; 1 0];
-sigy = [0 -1i; 1i 0];
-sigz = [1 0; 0 -1];
 
 Er = double.empty(length(kx), length(ky), 2, 0);
 for ii = 1:length(kx)
@@ -38,24 +74,12 @@ for ii = 1:length(kx)
         end
         
         for kk = 1:length(b) % N.N.N. contribution
-            Hr3 = Hr3 + sin(kv*b{kk}');
+            Hr3 = Hr3 + 2*tnnn*sin(kv*b{kk}');
         end
         
-        Hr = tnn*(sigx.*Hr1 - sigy.*Hr2) + sigz*(mass + tnnn*Hr3); % total hamiltonian
+        Hr = tnn*(sigx.*Hr1 - sigy.*Hr2) + sigz*(mass + Hr3); % total hamiltonian
         [~, ee] = eig(Hr);
         Er(ii,jj,:,1) = diag(ee);
     end
 end
-
-figure
-box on
-hold on
-uband = surf(KX, KY, squeeze(Er(:,:,1)));
-uband.EdgeColor = 'none';
-lband = surf(KX, KY, squeeze(Er(:,:,2)));
-lband.EdgeColor = 'none';
-xlabel('$\vec{k}_x$','Interpreter','latex')
-ylabel('$\vec{k}_y$','Interpreter','latex')
-zlabel('Energy')
-set(gca,'FontSize',14)
-view(30,45)
+end
