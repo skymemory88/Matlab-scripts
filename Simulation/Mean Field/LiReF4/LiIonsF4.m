fe568404cc01ef10a875d546d525cb11cbd245e3
skymemory88@hotmail.com <skymemory88@hotmail.com>
2022-06-08 14:31:52 +0200

    remove redundant matrix dimensions

--- Simulation/Mean Field/LiReF4/LiIonsF4.m
+++ Simulation/Mean Field/LiReF4/LiIonsF4.m
@@ -9,28 +9,13 @@ global strategies rundipole Options;
 t = tic;
     
 % Matrix used to calculate order parameters from (alternating) moments
-alt=[1  1  1
-    -1  1  1
-    -1 -1  1
-     1 -1  1];  
+alt = [1  1  1
+      -1  1  1
+      -1 -1  1
+       1 -1  1];  
 
 ion_mom_init = ion.mom;
- 
-ion.Jmom=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
-ion.Jmom_norm=zeros([length(temp),size(field,2),size(ion.name,1)]);
-ion.Jmom_hyp=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
-ion.Jmom_hyp_norm=zeros([length(temp),size(field,2),size(ion.name,1)]);
-
-ion.altJmom=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
-ion.altJmom_hyp=zeros([3,length(temp),size(field,2),size(ion.name,1)]);
-
-ion.Js=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
-ion.Js_hyp=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
-
-ion.altJs=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
-ion.altJs_hyp=zeros([4,3,length(temp),size(field,2),size(ion.name,1)]);
-
-history=cell([length(temp),size(field,2)]);
+history = cell([length(temp),size(field,2)]);
 
 idx = find(ion.prop);
 if ion.hyp(idx) > 0
@@ -38,8 +23,6 @@ if ion.hyp(idx) > 0
 elseif ion.hyp(idx) == 0
     H_dims = 2*ion.J(idx)+1; % Claculate the dimension of the Hilbert space
 end
-E = zeros(size(field,2),length(temp),H_dims);
-V = zeros(size(field,2),length(temp),H_dims,H_dims);
 
 switch Options.scanMode % determine x-variable and data slicing direction
     case 'field'
@@ -49,99 +32,95 @@ switch Options.scanMode % determine x-variable and data slicing direction
         continu_var = temp;
         discrt_var = vecnorm(field,2);
 end
+E = zeros(length(continu_var),H_dims);
+V = zeros(length(continu_var),H_dims,H_dims);
 
-for j = 1:length(discrt_var)
+for ii = 1:length(discrt_var)
     % Reinitiate the angular moments at each temperature step (2020.01.02)
     if length(continu_var) > 1
         ion.mom = ion_mom_init; %Reinitialize the spin moments at each field
         ion.mom_hyp = ion.mom;
         rundipole = true;
     end
-    for i = 1:length(continu_var)
+    ion.Jmom = zeros([3,length(continu_var),size(ion.name,1)]);
+    ion.Jmom_norm = zeros([length(continu_var),size(ion.name,1)]);
+    ion.Jmom_hyp = zeros([3,length(continu_var),size(ion.name,1)]);
+    ion.Jmom_hyp_norm = zeros([length(continu_var),size(ion.name,1)]);
+
+    ion.altJmom = zeros([3,length(continu_var),size(ion.name,1)]);
+    ion.altJmom_hyp = zeros([3,length(continu_var),size(ion.name,1)]);
+
+    ion.Js = zeros([4,3,length(continu_var),size(ion.name,1)]);
+    ion.Js_hyp = zeros([4,3,length(continu_var),size(ion.name,1)]);
+
+    ion.altJs = zeros([4,3,length(continu_var),size(ion.name,1)]);
+    ion.altJs_hyp = zeros([4,3,length(continu_var),size(ion.name,1)]);
+    for jj = 1:length(continu_var)
         switch Options.scanMode % determine x-variable and data slicing direction
             case 'field'
-                Bidx = i;
-                Tidx = j;
+                [ion,evolution,energy,v,~] = remf(ion,field(:,jj)',temp(ii),demagn,alpha);
             case 'temp'
-                Bidx = j;
-                Tidx = i;
-        end
-        [ion,evolution,energy,v,~] = remf(ion,field(:,Bidx)',temp(Tidx),demagn,alpha);        
-        for k = 1:size(ion.name,1)
-            ion.altJmom(:,Tidx,Bidx,k) = mean(alt.*ion.mom(:,:,k));
-            ion.altJmom_hyp(:,Tidx,Bidx,k) = mean(alt.*ion.mom_hyp(:,:,k));
-            ion.Jmom(:,Tidx,Bidx,k) = mean(ion.mom(:,:,k));
-            ion.Jmom_hyp(:,Tidx,Bidx,k) = mean(ion.mom_hyp(:,:,k)); 
+                [ion,evolution,energy,v,~] = remf(ion,field(:,ii)',temp(jj),demagn,alpha);
+        end    
+        for kk = 1:size(ion.name,1)
+            ion.altJmom(:,jj,kk) = mean(alt.*ion.mom(:,:,kk));
+            ion.altJmom_hyp(:,jj,kk) = mean(alt.*ion.mom_hyp(:,:,kk));
+            ion.Jmom(:,jj,kk) = mean(ion.mom(:,:,kk));
+            ion.Jmom_hyp(:,jj,kk) = mean(ion.mom_hyp(:,:,kk)); 
 %             ion.Jmom_norm(:,j,i,k) = norm(ion.mom(1,:,k)); % from originial code--Yikai (11.02.2020)
-            ion.Jmom_norm(Tidx,Bidx,k) = norm(ion.Jmom(:,Tidx,Bidx,k));
-            ion.Jmom_hyp_norm(Tidx,Bidx,k) = norm(ion.Jmom_hyp(:,Tidx,Bidx,k)); % addded on 11.02.2020 --Yikai
-            ion.Js(:,:,Tidx,Bidx,k) = ion.mom(:,:,k);
-            ion.Js_hyp(:,:,Tidx,Bidx,k) = ion.mom_hyp(:,:,k);
-            ion.altJs(:,:,Tidx,Bidx,k) = alt.*ion.mom(:,:,k);
-            ion.altJs_hyp(:,:,Tidx,Bidx,k) = alt.*ion.mom_hyp(:,:,k);        
-            history{Tidx,Bidx} = evolution;
+            ion.Jmom_norm(jj,kk) = norm(ion.Jmom(:,jj,kk));
+            ion.Jmom_hyp_norm(jj,kk) = norm(ion.Jmom_hyp(:,jj,kk)); % addded on 11.02.2020 --Yikai
+            ion.Js(:,:,jj,kk) = ion.mom(:,:,kk);
+            ion.Js_hyp(:,:,jj,kk) = ion.mom_hyp(:,:,kk);
+            ion.altJs(:,:,jj,kk) = alt.*ion.mom(:,:,kk);
+            ion.altJs_hyp(:,:,jj,kk) = alt.*ion.mom_hyp(:,:,kk);        
+            history{ii,jj} = evolution;
         end
-        
-        if strategies.powerlaw && Tidx>3 && Tidx<length(temp) % Guess starting values for next temperature using powerlaw
-            beta=0.5; % assume exponent 1/2. Could generalize to use more points and flexible exponent
-            inv_beta=1/beta;
-            J1=zeros([4,3,size(ion.name,1)]);
-            J2=zeros([4,3,size(ion.name,1)]);
-            Tc=zeros([4,3,size(ion.name,1)]);
-            Aa=zeros([4,3,size(ion.name,1)]);
-            Jnew=zeros([4,3,size(ion.name,1)]);
-            for k=1:size(ion.name,1)
-                J1(:,:,k)=squeeze(history{Tidx,Bidx}.(ion.name{k})(end,:,:));
-                J2(:,:,k)=squeeze(history{Tidx-1,Bidx}.(ion.name{k})(end,:,:));
-                Tc(:,:,k)=((J1(:,:,k)./J2(:,:,k)).^inv_beta*temp(Tidx-1)-temp(Tidx))./((J1(:,:,k)./J2(:,:,k)).^inv_beta-1); % can generalize to use more points
-                if isempty(find(((Tc(:,:,k)<=0) + (Tc(:,:,k) > temp(end))),1)) % only do this if Tc guess is reasonable
-                    Aa(:,:,k)=J2(:,:,k).^inv_beta./(Tc(:,:,k)-temp(Tidx-1));
-                    if isempty(find(~isfinite(Aa(:,:,k))+isnan(Aa(:,:,k)),1)) % problem if Tc happens to equal temp(j-1)
-                        Jnew(:,:,k)=real((Aa(:,:,k).*(Tc(:,:,k)-temp(Tidx+1))).^beta).*(temp(Tidx+1)<Tc(:,:,k));
-                        ion.mom(:,:,k)=Jnew(:,:,k);
+        if strcmp(Options.scanMode, 'field') % Guess starting values for next temperature using powerlaw
+            if strategies.powerlaw && ii < length(discrt_var)
+                beta = 0.5; % assume exponent 1/2. Could generalize to use more points and flexible exponent
+                inv_beta = 1/beta;
+                J1 = zeros([4,3,size(ion.name,1)]);
+                J2 = zeros([4,3,size(ion.name,1)]);
+                Tc = zeros([4,3,size(ion.name,1)]);
+                Aa = zeros([4,3,size(ion.name,1)]);
+                Jnew = zeros([4,3,size(ion.name,1)]);
+                for kk = 1:size(ion.name,1)
+                    J1(:,:,kk) = squeeze(history{ii,jj}.(ion.name{kk})(end,:,:));
+                    J2(:,:,kk) = squeeze(history{ii-1,jj}.(ion.name{kk})(end,:,:));
+                    Tc(:,:,kk) = ((J1(:,:,kk)./J2(:,:,kk)).^inv_beta*temp(ii-1)-temp(ii))./((J1(:,:,kk)./J2(:,:,kk)).^inv_beta-1); % can generalize to use more points
+                    if isempty(find(((Tc(:,:,kk) <= 0) + (Tc(:,:,kk) > temp(end))),1)) % only do this if Tc guess is reasonable
+                        Aa(:,:,kk) = J2(:,:,kk).^inv_beta./(Tc(:,:,kk)-temp(ii-1));
+                        if isempty(find(~isfinite(Aa(:,:,kk))+isnan(Aa(:,:,kk)),1)) % problem if Tc happens to equal temp(j-1)
+                            Jnew(:,:,kk) = real((Aa(:,:,kk).*(Tc(:,:,kk)-temp(ii+1))).^beta).*(temp(ii+1)<Tc(:,:,kk));
+                            ion.mom(:,:,kk) = Jnew(:,:,kk);
+                        end
                     end
                 end
             end
         end
-        E(Bidx,Tidx,:) = energy; % Eigen energy
-        V(Bidx,Tidx,:,:) = v; % Eigen function
+        E(jj,:) = energy; % Eigen energy
+        V(jj,:,:) = v; % Eigen function
     end
+    eee = squeeze(E(:,:));
+    vvv = squeeze(V(:,:,:));
 % Save the data split by temperatures when they are multi-dimensional, otherwise save the data outside this function
-    if length(discrt_var) >1 && length(continu_var) >1
+    if length(discrt_var) > 1 && length(continu_var) > 1
         switch Options.scanMode
             case 'field'
-                eee = squeeze(E(:,j,:));
-                vvv = squeeze(V(:,j,:,:));
-                ttt = temp(Tidx);
+                ttt = temp(ii);
                 fff = field;
-                tit=strcat('Hscan_Li',ion.name(ion.prop~=0), sprintf('F4_%1$3.3fK_%2$.2fDg_%3$.1fDg_hp=%4$.2f.mat',...
-                    discrt_var(j), theta*180/pi, phi*180/pi,ion.hyp(idx)));
+                tit = strcat('Hscan_Li',ion.name(ion.prop ~= 0), sprintf('F4_%1$3.3fK_%2$.2fDg_%3$.1fDg_hp=%4$.2f.mat',...
+                    discrt_var(ii), theta*180/pi, phi*180/pi,ion.hyp(idx)));
                 save(fullfile(Options.filepath,char(tit)),'ttt','fff','eee','vvv','ion','-v7.3')
             case 'temp'
-                eee = squeeze(E(j,:,:));
-                vvv = squeeze(V(j,:,:,:));
                 ttt = temp;
-                fff = field(:,Bidx);
-                tit=strcat('Tscan_Li',ion.name(ion.prop~=0), sprintf('F4_%1$3.3fT_%2$.2fDg_%3$.1fDg_hp=%4$.2f.mat',...
-                    discrt_var(j), theta*180/pi, phi*180/pi,ion.hyp(idx)));
+                fff = field(:,ii);
+                tit = strcat('Tscan_Li',ion.name(ion.prop ~= 0), sprintf('F4_%1$3.3fT_%2$.2fDg_%3$.1fDg_hp=%4$.2f.mat',...
+                    discrt_var(ii), theta*180/pi, phi*180/pi,ion.hyp(idx)));
                 save(fullfile(Options.filepath,char(tit)),'ttt','fff','eee','vvv','ion','-v7.3')
         end
     end
 end
-
-ion.Jmom = squeeze(ion.Jmom(:,:,:,:));
-ion.Jmom_hyp = squeeze(ion.Jmom_hyp(:,:,:,:));
-
-ion.Jmom_norm = squeeze(ion.Jmom_norm(:,:,:));
-ion.Jmom_hyp_norm = squeeze(ion.Jmom_hyp_norm(:,:,:));
-
-ion.altJmom = squeeze(ion.altJmom(:,:,:,:));
-ion.altJmom_hyp = squeeze(ion.altJmom_hyp(:,:,:,:));
-
-ion.Js = squeeze(ion.Js(:,:,:,:,:));
-ion.Js_hyp = squeeze(ion.Js_hyp(:,:,:,:,:));
-
-ion.altJs = squeeze(ion.altJs(:,:,:,:,:));
-ion.altJs_hyp = squeeze(ion.altJs_hyp(:,:,:,:,:));
 
 toc(t)
