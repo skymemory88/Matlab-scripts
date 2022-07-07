2c51b503580e4d8b10d8d90a25e69952fd685083
skymemory88@hotmail.com <skymemory88@hotmail.com>
2022-07-07 11:46:28 +0200

    add legend, color, and marker options

--- Plot functions/Fieldscan/off_res_chi.m
+++ Plot functions/Fieldscan/off_res_chi.m
@@ -2,13 +2,25 @@ Options.RPA  = true;
 Options.nZee = false; % Nuclear Zeeman interaction
 Options.sample = 239; % Sample to use (SC###)
 
-temp = [0.1 0.3 0.8 1]; % temperatures
+temps = [0.1 0.3 0.6 1.2]; % temperatures
 % temp = 1;
-theta = 0.0; % a-c plane rotation
-phi = 2.3; % a-b plane rotation
+theta = 0.0; % c-axis deviation
+phi = 2.3; % in-plane (a-b) rotation
 freq_l = 4.70; % frequency range lower limit [GHz]
 freq_h = 4.745; % frequency range upper limit [GHz]
 gama = 1e-4; % default spin linewidth
+
+mkr = ['o','s','^','x','<','>']; % marker style pool
+lin = ['-','--','-.',':']; % line style pool
+mc = [[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; ...
+    [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880];[0.3010, 0.7450, 0.9330]]; % marker color pool
+
+lgd_sim = string(temps.*1000); % legends for calculated data
+lgd_sim = 'Sim.' + lgd_sim + ' mK';
+lgd_sim = lgd_sim';
+% lgd_sim = strings(length(temps),1); % container for legends for simulated data
+exp_temps = double.empty(length(temps),0); % container for legends for experimental data
+
 
 hbar = 1.05457E-34; % Reduced Planck constant [J.s/rad]
 meV2J = 1.60218e-22; % [J/meV]
@@ -19,14 +31,14 @@ exp_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 projec
     num2str(Options.sample),'\'];
 
 dat_lst = readtable(fullfile(exp_loc, "off_res_list.xlsx"));
-exp_fold = table2array(dat_lst(:,1));
-exp_files = table2array(dat_lst(:,2));
-exp_temps = table2array(dat_lst(:,3));
+folder = table2array(dat_lst(:,1));
+efiles = table2array(dat_lst(:,2));
+etemps = table2array(dat_lst(:,3));
 
 % set calculated data loading parameters
 if Options.nZee == true
     sim_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Simulations\Matlab\',...
-        'Susceptibilities\with Hz_I\backup\'];
+        'Susceptibilities\with Hz_I\'];
 else
     sim_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Simulations\Matlab\',...
         'Susceptibilities\without Hz_I\backup\'];
@@ -36,16 +48,20 @@ end
 chi_fig = figure;
 box on
 hold on
-for ii = 1:length(temp)
-    [~, idx] = min(abs(exp_temps-temp(ii))); % find the closest experimental temperature
-    exp_file = fullfile([exp_loc, exp_fold{idx},'\'], exp_files{idx}); % locate the experimental data
+for ii = 1:length(temps)
+    [~, idx] = min(abs(etemps-temps(ii))); % find the closest experimental temperature
+    exp_file = fullfile([exp_loc, folder{idx},'\'], efiles{idx}); % locate the experimental data
     load([exp_file, '_interp'], '-mat', 'analysis', 'continu_var');
     fc = nonzeros(analysis.wc);
     fc = fc(1); % pick zero-field resonant frequency as the reference frequency
-    figure(chi_fig)
-    plot(continu_var(1,:), analysis.xr, '.r'); % plot the experimental data
+    exp_temps(ii,1) = etemps(idx); % record the actual experimental temperatures
 
-    sim_file = sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_%4$.2e_4*GHz', temp(ii), theta, phi, gama);
+
+    figure(chi_fig)
+    scatter(continu_var(1,1:10:end), analysis.xr(1:10:end), 'Marker', mkr(ii),...
+        'MarkerEdgeColor', mc(ii,:), 'MarkerFaceColor', mc(ii,:)); % plot the experimental data
+
+    sim_file = sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_%4$.2e_4*GHz', temps(ii), theta, phi, gama);
     if Options.RPA == true
         sim_load = strcat('RPA_LiHoF4_', sim_file, '.mat');
         xlab = 'Re[\chi^{zz}_{RPA}] (meV)';
@@ -67,9 +83,17 @@ for ii = 1:length(temp)
     end
     
     [~, fidx] = min(abs(freq_total - fc));
-    figure(chi_fig)
-    plot(fields, real(chiz(fidx,:))/ConvUnit,'-k','lineWidth',1.5);
+    figure(chi_fig);
+    plot(fields, real(chiz(fidx,:))/ConvUnit, '-','Color', mc(ii,:), 'lineWidth', 1.5);
 end
+
+lgd_exp = string(exp_temps.*1000); % legends for experimental data
+lgd_exp = 'Exp.' + lgd_exp + ' mK';
+lgd = [lgd_exp lgd_sim]';
+lgd = lgd(:); % interweaved legends
+
+legend(lgd, 'Location', 'northwest')
 xlim([0 9])
+title('Easy axis susceptibilities')
 xlabel('Magnetick Field (T)');
 ylabel(xlab);
