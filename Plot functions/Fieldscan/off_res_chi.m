83d166d62d70389acf02d012aa1a7b12e3c0753d
skymemory88@hotmail.com <skymemory88@hotmail.com>
2022-07-26 15:15:22 +0200

    plot option update

--- Plot functions/Fieldscan/off_res_chi.m
+++ Plot functions/Fieldscan/off_res_chi.m
@@ -2,13 +2,17 @@ Options.RPA  = true;
 Options.nZee = false; % Nuclear Zeeman interaction
 Options.sample = 239; % Sample to use (SC###)
 
-temps = [0.1 0.3 0.8 1.2]; % temperatures
+hbar = 1.05457E-34; % Reduced Planck constant [J.s/rad]
+meV2J = 1.60218e-22; % [J/meV]
+Gh2mV = hbar * 2*pi / meV2J * 10^9/1.05; % [meV/GHz]
+
+temps = [0.1 0.3 0.5 0.8 1.0]; % temperatures
 % temp = 1;
 theta = 0.0; % c-axis deviation
-phi = 2.3; % in-plane (a-b) rotation
+phi = 4.0; % in-plane (a-b) rotation
 freq_l = 4.70; % frequency range lower limit [GHz]
 freq_h = 4.745; % frequency range upper limit [GHz]
-gama = 1e-4; % default spin linewidth
+gama = 9e-5; % default spin linewidth
 
 mkr = ['o','s','^','x','<','>']; % marker style pool
 lin = ['-','--','-.',':']; % line style pool
@@ -20,11 +24,6 @@ lgd_sim = 'Sim.' + lgd_sim + ' mK';
 lgd_sim = lgd_sim';
 % lgd_sim = strings(length(temps),1); % container for legends for simulated data
 exp_temps = double.empty(length(temps),0); % container for legends for experimental data
-
-
-hbar = 1.05457E-34; % Reduced Planck constant [J.s/rad]
-meV2J = 1.60218e-22; % [J/meV]
-Gh2mV = hbar * 2*pi / meV2J * 10^9; % [meV/GHz]
 
 % set experimental data loading parameters
 exp_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC',...
@@ -41,7 +40,7 @@ if Options.nZee == true
         'Susceptibilities\with Hz_I\'];
 else
     sim_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Simulations\Matlab\',...
-        'Susceptibilities\without Hz_I\'];
+        'Susceptibilities\without Hz_I\CEF_2015_low_R=0.800\'];
 end
 
 % plot the data
@@ -63,13 +62,17 @@ for ii = 1:length(temps)
 
     sim_file = sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_%4$.2e_4*GHz', temps(ii), theta, phi, gama);
     if Options.RPA == true
-        sim_load = strcat('RPA_LiHoF4_', sim_file, '.mat');
+        sim_load = strcat("RPA_LiHoF4_", sim_file, ".mat");
         xlab = 'Re[\chi^{zz}_{RPA}] (meV)';
     else
-        sim_load = strcat('chi0_LiHoF4_', sim_file, '.mat');
+        sim_load = strcat("chi0_LiHoF4_", sim_file, ".mat");
         xlab = 'Re[\chi^{zz}_{MF}] (meV)';
     end
     chi_file = dir(fullfile(sim_loc, sim_load)); % use parts unspecified file name
+    if isempty(chi_file)
+        disp('Cannot match the file, check the name and directory!')
+        break
+    end
     load([sim_loc chi_file.name],'-mat','freq_total','fields','chi','unit'); % load calculated suscpetibilities
     chiz = squeeze(chi(3,3,:,:));
 
