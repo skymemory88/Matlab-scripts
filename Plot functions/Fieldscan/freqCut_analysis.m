f587e34c29ee59874d52830dd4933e51d3c1aa9c
skymemory88@hotmail.com <skymemory88@hotmail.com>
2022-08-26 23:35:10 +0200

    update

--- Plot functions/Fieldscan/freqCut_analysis.m
+++ Plot functions/Fieldscan/freqCut_analysis.m
@@ -1,17 +1,23 @@
 %% isolated frequency sweep plotter
-
-B0 = [0.1 4.916 5.89]; % field location of frequency cuts to plot/fit
+clearvars
+B0 = [0.1 4.23 7.0]; % field location of frequency cuts to plot/fit
 bidx = uint32.empty(length(B0),0);
 % load the processed data
-Option.plot = 'compressed'; % plot the frequency cuts onto the same or individual plots
-Option.load_method = 'manual'; % 1: auto, 2: manual, 3: skip (if already exist in workspace). 
-    temp = 0.130; % temperature (K) for locating the file in 'auto' mode
+Options.plot = 'compressed'; % plot the frequency sweeps onto the same or individual plots
+Options.yScale = 'dB'; % 1. 'lin': linear. 2. 'dB': log scale
+Options.fit = true; % Option to fit the data
+    dType = 'off'; % specify the data type (on/off resonance) for fitting function selection
+Options.load_method = 'manual'; % 1: auto, 2: manual, 3: skip (if already exist in workspace). 
+    temp = 0.500; % temperature (K) for locating the file in 'auto' mode
     sample = 239; % sample SC number for locating the file in 'auto' mode
 
-switch Option.load_method
+mc = {[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; ...
+    [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880];[0.3010, 0.7450, 0.9330]}; % marker color pool
+
+switch Options.load_method
     case 'auto'
         exp_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC',...
-            num2str(Options.sample), '\'];
+            num2str(sample), '\'];
         dat_lst = readtable(fullfile(exp_loc, sprintf("SC%u_off_list.xlsx", sample)));
         folder = table2array(dat_lst(:,1));
         efiles = table2array(dat_lst(:,2));
@@ -20,11 +26,18 @@ switch Option.load_method
         exp_file = fullfile([exp_loc, folder{idx},'\'], efiles{idx}); % locate the experimental data
         load([exp_file, '_interp'], '-mat', 'S11', 'freq', 'continu_var', 'analysis');
     case 'manual'
-        fileloc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\',...
-            'SC239\2021.07.16'];
-        loadname = '2021_07_0015';
+        fileloc = ['G:\.shortcut-targets-by-id\1CapZB_um4grXCRbK6t_9FxyzYQn8ecQE\',...
+            'File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\',...
+            'SC107 (4x5x2mm)\19.05.2019'];
+%         fileloc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\',...
+%             'SC239\2021.07.16'];
+        loadname = '2019_05_0026';
         LoadObj = fullfile(fileloc,loadname);
         load([LoadObj, '_interp'], '-mat', 'S11', 'freq', 'continu_var', 'analysis');
+        for ii = 1:size(S11,2)
+            S11(:,ii) = S11(:,ii)./max(S11(:,ii));
+%             S11(:,ii) = S11(:,ii)./0.9;
+        end
 end
 
 % define colormap
@@ -33,29 +46,71 @@ cmap = unique([[0 0 0];[zeros(200,1),linspace(0,0.5,200)',linspace(0.4,1,200)'];
     [ones(200,1),linspace(0.5,1,200)',linspace(0,1,200)']],'rows');
 cmap = flip(cmap,1);
 
-sfig = figure('Colormap', cmap);
+sfig = figure('Colormap', cmap, 'Position',[200 200 800 650]);
 box on
 hold on
 cplot = pcolor(continu_var, freq, mag2db(S11));
 cplot.EdgeColor = 'none';
+c = colorbar('Location','northoutside');
+pos = get(gca,'Position');
+set(c,'Position',[pos(1) pos(2)+pos(4) pos(3)-0.1 0.05]);
+set(gca,'Position',[pos(1) pos(2) pos(3) pos(4)-0.01])
+c.Label.String = '|S11|';
+c.Label.Position = [pos(3)+1.5 0.2];
+set(gca, 'FontSize', 14)
 xlabel('Magnetic Field (T)')
 ylabel('Frequency (GHz)')
 
+if Options.fit == true
+    [~, idx0] = min(continu_var(1,:));
+    weight(:,:,1) = abs(1./S11(:,:));      % Weight function option 1
+    % weight = 1./gradientweight(S11);       % Weight function option 2
+    % weight = ones(size(S11));                % Weight function option 3 (uniform)
+    weight(isinf(weight)) = 1e4;           % Remove infinities in weight function
+    param  =  [1e-3    1e-4    0     0    freq(S11(:,idx0)==min(S11(:,idx0)),1)]; % param = {'kpe', 'kpi', 'Re[chi]', 'Im[chi]', 'wc'}
+    bound_l = [ 0       0      0     0    min(freq(:,idx0))]; % lower bound of fitting parameters
+    bound_h = [Inf     Inf     0     0    max(freq(:,idx0))]; % upper bound of fitting parameters
+    fit0 = iptoptx(freq(:,idx0), S11(:,idx0), continu_var(1,idx0), param, bound_l, bound_h, weight(:,idx0), false);
+    kpe0 = fit0.kpe;
+    kpi0 = fit0.kpi;
+    f_init = fit0.wc;
+end
+
+S11_fit = cell(length(B0),1);
 for ii = 1:length(B0)
     [~, bidx(ii)] = min(abs(B0(ii)-continu_var(1,:)));
     B0(ii) = continu_var(1,bidx(ii));
     figure(sfig)
-    plot([B0(ii) B0(ii)], [min(freq(:,ii)) max(freq(:,ii))], '--w')
+    plot([B0(ii) B0(ii)], [min(freq(:,ii)) max(freq(:,ii))], '--w', 'LineWidth', 2)
+
+    if Options.fit == true
+        % Fit using input-output formalism
+        param  =  [kpe0  kpi0     0      0    f_init]; % param = {'kpe', 'kpi', 'Re[chi]', 'Im[chi]', 'wc'}
+        bound_l = [kpe0-0.1  kpi0   -Inf   -Inf   f_init]; % lower bound of fitting parameters
+        bound_h = [kpe0+0.1  kpi0    Inf    Inf   f_init]; % upper bound of fitting parameters
+        S11_fit{ii} = iptoptx(freq(:,bidx(ii)), S11(:,bidx(ii)), B0(ii), param, bound_l, bound_h, weight(:,bidx(ii)), false);
+    end
 end
 
-switch Option.plot
+switch Options.plot
     case 'compressed'
-        fig = figure('Position',[300 50 350 650]);
+        fig = figure('Position',[1000 200 350 650]);
         box on
         hold on
         for ii = 1:length(B0)
-            scatter( freq(:,bidx(ii)), mag2db(S11(:,bidx(ii))), 'o', 'filled');
+            switch Options.yScale
+                case 'dB'
+                    yDat = mag2db(S11(:,bidx(ii)));
+                    yFit = mag2db(feval(S11_fit{ii},freq(:,bidx(ii))));
+                case 'lin'
+                    yDat = S11(:,bidx(ii));
+                    yFit = feval(S11_fit{ii},freq(:,bidx(ii)));
+            end
+            scatter( freq(:,bidx(ii)), yDat, 'o', 'filled');
+%             plot(freq(:,bidx(ii)), yFit, '-', 'Color', mc{2});
+            plot(freq(:,bidx(ii)), yFit, '--k', 'LineWidth', 2);
         end
+        set(gca, 'FontSize', 14)
         xlabel('Frequency (GHz)')
         ylabel('|S11| (dB)')
         camroll(90)
@@ -63,13 +118,33 @@ switch Option.plot
     case 'individual'
         fig = gobjects(length(B0),2);
         for ii = 1:length(B0)
-            fig(ii) = figure('Position',[300 50 350 650]);
+            fig(ii) = figure('Position',[1000 200 350 650]);
             box on
             hold on
-            scatter( freq(:,bidx(ii)), mag2db(S11(:,bidx(ii))), 'o', 'filled');
+            switch Options.yScale
+                case 'dB'
+                    yDat = mag2db(S11(:,bidx(ii)));
+                    yFit = mag2db(feval(S11_fit{ii},freq(:,bidx(ii))));
+                case 'lin'
+                    yDat = S11(:,bidx(ii));
+                    yFit = feval(S11_fit{ii},freq(:,bidx(ii)));
+            end
+            scatter( freq(:,bidx(ii)), yDat, 'o', 'filled');
+            plot(freq(:,bidx(ii)), yFit, '-', 'Color', mc{2});
+            set(gca, 'FontSize', 14)
             xlabel('Frequency (GHz)')
             ylabel('|S11| (dB)')
             camroll(90)
             set(gca,'FontSize',14)
         end
 end
+
+figure;
+plot(continu_var(1,:),analysis.xr,'.');
+hold on
+ylabel('Re[\chi]')
+xlabel('Magnetic Field (T)');
+yyaxis right
+plot(continu_var(1,:),analysis.xi,'.');
+ylabel('Im[\chi]')
+set(gca,'FontSize',14)
--- Plot functions/Fieldscan/off_res_chi.m
+++ Plot functions/Fieldscan/off_res_chi.m
@@ -6,18 +6,18 @@ hbar = 1.05457E-34; % Reduced Planck constant [J.s/rad]
 meV2J = 1.60218e-22; % [J/meV]
 Gh2mV = hbar * 2*pi / meV2J * 10^9/1.05; % [meV/GHz]
 
-temps = [0.1 0.3 0.5 0.8 1.0]; % temperatures
+temps = [0.1 0.3 0.5 0.8]; % temperatures
 % temp = 1;
 theta = 0.0; % c-axis deviation
-phi = 4.0; % in-plane (a-b) rotation
-freq_l = 4.70; % frequency range lower limit [GHz]
+phi = 2.3; % in-plane (a-b) rotation
+freq_l = 4.700; % frequency range lower limit [GHz]
 freq_h = 4.745; % frequency range upper limit [GHz]
-gama = 9e-5; % default spin linewidth
+gama = 1e-4; % default spin linewidth
 
 mkr = ['o','s','^','x','<','>']; % marker style pool
 lin = ['-','--','-.',':']; % line style pool
-mc = [[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; ...
-    [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880];[0.3010, 0.7450, 0.9330]]; % marker color pool
+mc = {[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; ...
+    [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880];[0.3010, 0.7450, 0.9330]}; % marker color pool
 
 lgd_sim = string(temps.*1000); % legends for calculated data
 lgd_sim = 'Sim.' + lgd_sim + ' mK';
@@ -40,11 +40,14 @@ if Options.nZee == true
         'Susceptibilities\with Hz_I\'];
 else
     sim_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Simulations\Matlab\',...
-        'Susceptibilities\without Hz_I\CEF_2015_low_R=0.800\'];
+        'Susceptibilities\without Hz_I\CEF_2015_mod_R=0.785_phi=2.3\'];
 end
 
 % plot the data
-chi_fig = figure;
+chir_fig = figure;
+box on
+hold on
+chii_fig = figure;
 box on
 hold on
 for ii = 1:length(temps)
@@ -56,17 +59,26 @@ for ii = 1:length(temps)
     exp_temps(ii,1) = etemps(idx); % record the actual experimental temperatures
 
 
-    figure(chi_fig)
-    scatter(continu_var(1,1:10:end), analysis.xr(1:10:end), 'Marker', mkr(mod(ii,length(mkr))),...
-        'MarkerEdgeColor', mc(mod(ii,size(mc,1)),:), 'MarkerFaceColor', mc(mod(ii,size(mc,1)),:)); % plot the experimental data
+    figure(chir_fig)
+    scatter(continu_var(1,1:15:end), analysis.xr(1:15:end), 'Marker', mkr(mod(ii,length(mkr))),...
+        'MarkerEdgeColor', mc{mod(ii,size(mc,1))}, 'MarkerFaceColor', mc{mod(ii,size(mc,1))}); % plot the experimental data
+
+    figure(chii_fig)
+    scatter(continu_var, medfilt1(analysis.xi), 'Marker', mkr(mod(ii,length(mkr))),...
+        'MarkerEdgeColor', mc{mod(ii,size(mc,1))}, 'MarkerFaceColor', mc{mod(ii,size(mc,1))}); % plot the experimental data
+%     figure(chii_fig)
+%     plot(continu_var(1,1:15:end), analysis.xi(1:15:end), 'LineStyle', '-', 'Marker', mkr(mod(ii,length(mkr))),...
+%         'Color', mc{mod(ii,size(mc,1))}); % plot the experimental data
 
     sim_file = sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_%4$.2e_4*GHz', temps(ii), theta, phi, gama);
     if Options.RPA == true
         sim_load = strcat("RPA_LiHoF4_", sim_file, ".mat");
-        xlab = 'Re[\chi^{zz}_{RPA}] (meV)';
+        xlabr = 'Re[\chi^{zz}_{RPA}] (meV)';
+        xlabi = 'Im[\chi^{zz}_{RPA}] (meV)';
     else
         sim_load = strcat("chi0_LiHoF4_", sim_file, ".mat");
-        xlab = 'Re[\chi^{zz}_{MF}] (meV)';
+        xlabr = 'Re[\chi^{zz}_{MF}] (meV)';
+        xlabi = 'Im[\chi^{zz}_{MF}] (meV)';
     end
     chi_file = dir(fullfile(sim_loc, sim_load)); % use parts unspecified file name
     if isempty(chi_file)
@@ -86,8 +98,11 @@ for ii = 1:length(temps)
     end
     
     [~, fidx] = min(abs(freq_total - fc));
-    figure(chi_fig);
-    plot(fields, real(chiz(fidx,:))/ConvUnit, 'LineStyle', lin(1), 'Color', mc(mod(ii,size(mc,1)),:), 'lineWidth', 1.5);
+    figure(chir_fig);
+    plot(fields, real(chiz(fidx,:))/ConvUnit, 'LineStyle', lin(1), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
+
+    figure(chii_fig);
+    plot(fields, imag(chiz(fidx,:))/ConvUnit, 'LineStyle', lin(1), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
 end
 
 lgd_exp = string(exp_temps.*1000); % legends for experimental data
@@ -95,8 +110,16 @@ lgd_exp = 'Exp.' + lgd_exp + ' mK';
 lgd = [lgd_exp lgd_sim]';
 lgd = lgd(:); % interweaved legends
 
+figure(chir_fig);
 legend(lgd, 'Location', 'northwest')
 xlim([0 9])
-title('Easy axis susceptibilities')
+title('Real part of AC susceptibilities (easy axis)')
 xlabel('Magnetic Field (T)');
-ylabel(xlab);
+ylabel(xlabr);
+
+figure(chii_fig);
+legend(lgd, 'Location', 'northwest')
+xlim([0 9])
+title('Imaginary part of AC susceptibilities (easy axis)')
+xlabel('Magnetic Field (T)');
+ylabel(xlabi);
