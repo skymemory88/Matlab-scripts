e2703b0b810cc2635944ddc11157fa09a337c250
skymemory88@hotmail.com <skymemory88@hotmail.com>
2023-07-05 13:57:43 +0100

    update

--- Plot functions/Fieldscan/off_res_chi.m
+++ Plot functions/Fieldscan/off_res_chi.m
@@ -1,47 +1,84 @@
-Options.RPA  = true;
+Options.RPA  = false;
+Options.filfctr = 0.0127*0.86; % filling factor correction
+% Options.filfctr = 0.112; % filling factor correction
+% Options.Bcorrt = 4.224/4.466; % field correction (default = 1)
 Options.nZee = false; % Nuclear Zeeman interaction
-Options.sample = 239; % Sample to use (SC###)
+Options.search = 'automatic'; % 1. 'Automatic' search and match or, 2. 'manual' appointment of files 
+    sample = 239; % Sample to use (SC###)
+    sim_temps = [0.15 0.3 0.5 0.8]; % temperatures
+    % sim_temps = 0.15;
+    theta = 0.0; % c-axis deviation
+    phi = 0.0; % in-plane (a-b) rotation
+    freq_l = 4.69; % frequency range lower limit [GHz]
+    freq_h = 4.745; % frequency range upper limit [GHz]
+    gama = 9e-5; % default spin linewidth
+    if strcmp(pathsep, ':')
+        platform = 'Unix';
+    else
+        platform = 'Win';
+    end
+switch Options.search
+    case 'automatic'
+        % set experimental data loading parameters
+        switch platform
+            case 'Win' % for Windows
+                exp_loc = ['G:\.shortcut-targets-by-id\1CapZB_um4grXCRbK6t_9FxyzYQn8ecQE\File sharing\PhD program\Research projects\',...
+                    'LiHoF4 project\Data\Experiment\LiHoF4\SC', num2str(sample), '\'];
+                sim_loc = ['G:\.shortcut-targets-by-id\1CapZB_um4grXCRbK6t_9FxyzYQn8ecQE\File sharing\PhD program\',...
+                    'Research projects\LiHoF4 project\Data\Simulations\Matlab\Susceptibilities\'];
+                if Options.nZee == true
+                    sim_loc = [sim_loc,'Hz_I=1\'];
+                else
+                    sim_loc = [sim_loc, 'Hz_I=0\'];
+                end
+            case 'Unix' % for Mac/Linux
+                exp_loc = ['/Users/yikaiyang/Library/CloudStorage/GoogleDrive-yikai.yang@epfl.ch/My Drive/File sharing/',...
+                    'PhD program/Research projects/LiHoF4 project/Data/Experiment/LiHoF4/SC', num2str(sample), '/'];
+                sim_loc = ['/Users/yikaiyang/Library/CloudStorage/GoogleDrive-yikai.yang@epfl.ch/My Drive/File sharing/',...
+                    'PhD program/Research projects/LiHoF4 project/Data/Simulations/MATLAB/Susceptibilities/'];
+                if Options.nZee == true
+                    sim_loc = [sim_loc,'Hz_I=1/'];
+                else
+                    sim_loc = [sim_loc, 'Hz_I=0/'];
+                end
+        end
+        
+        dat_lst = readtable(fullfile(exp_loc, sprintf("SC%u_off_list.xlsx", sample)));
+        folder = table2array(dat_lst(:,1));
+        efiles = table2array(dat_lst(:,2));
+        etemps = table2array(dat_lst(:,3));
+    case 'manual'
+        switch platform
+            case 'Win'
+                folder = ['G:\.shortcut-targets-by-id\1CapZB_um4grXCRbK6t_9FxyzYQn8ecQE\',...
+                    'File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\',...
+                    'SC107\19.05.2019\']; % for Windows
+            case 'Unix'
+                folder = ['/Users/yikaiyang/Library/CloudStorage/GoogleDrive-yikai.yang@epfl.ch/My Drive/File sharing/',...
+                    'PhD program/Research projects/LiHoF4 project/Data/Experiment/LiHoF4/',...
+                    'SC107/19.05.2019/']; % for Mac/Linux
+                efiles = '2019_05_0026';
+        end
+end
 
+muB = 9.274e-24; % Bohr magneton [J/T]
+mu0 = 4*pi*1e-7; % Vacuum permeability ([H/m])
+rho = 4/(5.175e-10 * 5.175e-10 * 10.750e-10); % magnetic moment number density in LiReF4 [m^-3]
 hbar = 1.05457E-34; % Reduced Planck constant [J.s/rad]
-meV2J = 1.60218e-22; % [J/meV]
-Gh2mV = hbar * 2*pi / meV2J * 10^9/1.05; % [meV/GHz]
-
-temps = [0.1 0.3 0.5 0.8]; % temperatures
-% temp = 1;
-theta = 0.0; % c-axis deviation
-phi = 2.3; % in-plane (a-b) rotation
-freq_l = 4.700; % frequency range lower limit [GHz]
-freq_h = 4.745; % frequency range upper limit [GHz]
-gama = 1e-4; % default spin linewidth
+J2meV = 6.24151e+21; % [meV/J]
+Gh2mV = hbar * 2*pi * 10^9 * J2meV; % [meV/GHz]
+ELEf = gLande(6,2) * muB * J2meV; % [meV/T]
 
 mkr = ['o','s','^','x','<','>']; % marker style pool
-lin = ['-','--','-.',':']; % line style pool
+lin = ["-", "--" , "-.", ":"]; % line style pool
 mc = {[0, 0.4470, 0.7410]; [0.8500, 0.3250, 0.0980]; [0.9290, 0.6940, 0.1250]; ...
     [0.4940, 0.1840, 0.5560]; [0.4660, 0.6740, 0.1880];[0.3010, 0.7450, 0.9330]}; % marker color pool
 
-lgd_sim = string(temps.*1000); % legends for calculated data
+lgd_sim = string(sim_temps.*1000); % legends for calculated data
 lgd_sim = 'Sim.' + lgd_sim + ' mK';
 lgd_sim = lgd_sim';
 % lgd_sim = strings(length(temps),1); % container for legends for simulated data
-exp_temps = double.empty(length(temps),0); % container for legends for experimental data
-
-% set experimental data loading parameters
-exp_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC',...
-    num2str(Options.sample),'\'];
-
-dat_lst = readtable(fullfile(exp_loc, sprintf("SC%u_off_list.xlsx", Options.sample)));
-folder = table2array(dat_lst(:,1));
-efiles = table2array(dat_lst(:,2));
-etemps = table2array(dat_lst(:,3));
-
-% set calculated data loading parameters
-if Options.nZee == true
-    sim_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Simulations\Matlab\',...
-        'Susceptibilities\with Hz_I\'];
-else
-    sim_loc = ['G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Simulations\Matlab\',...
-        'Susceptibilities\without Hz_I\CEF_2015_mod_R=0.785_phi=2.3\'];
-end
+exp_temps = double.empty(length(sim_temps),0); % container for legends for experimental data
 
 % plot the data
 chir_fig = figure;
@@ -50,59 +87,89 @@ hold on
 chii_fig = figure;
 box on
 hold on
-for ii = 1:length(temps)
-    [~, idx] = min(abs(etemps-temps(ii))); % find the closest experimental temperature
-    exp_file = fullfile([exp_loc, folder{idx},'\'], efiles{idx}); % locate the experimental data
-    load([exp_file, '_interp'], '-mat', 'analysis', 'continu_var');
-    fc = nonzeros(analysis.wc);
-    fc = fc(1); % pick zero-field resonant frequency as the reference frequency
-    exp_temps(ii,1) = etemps(idx); % record the actual experimental temperatures
-
-
+for ii = 1:length(sim_temps)
+    switch Options.search
+        case 'automatic'
+            [~, idx] = min(abs(etemps-sim_temps(ii))); % find the closest experimental temperature
+            exp_file = fullfile([exp_loc, folder{idx}], efiles{idx}); % locate the experimental data
+            exp_temps(ii,1) = etemps(idx); % record the actual experimental temperatures
+            load([exp_file, '_interp'], '-mat', 'analysis', 'continu_var');
+        case 'manual'
+            exp_file = fullfile(folder, efiles);
+            load([exp_file, '_interp'], '-mat', 'analysis', 'continu_var');
+            exp_temps(ii,1) = analysis.temp;
+    end
+    f0 = analysis.w0; % resonant frequency
+    B0 = analysis.Hs;
+    wc = nonzeros(analysis.wc); % cavity resonant frequency [GHz]
+    gw0 = sqrt(mu0 * 2*pi * wc(1)*1e9 * rho/2) * Options.filfctr; % susceptibility prefactor [T^2/J. rad/s]^1/2
+    gw2 = gw0^2 * 2*pi * 1e-9 / J2meV; % [GHz * T^2/meV]
+    
+    if ~isfield(Options,'Bcorrt'); Options.Bcorrt = 1; end
     figure(chir_fig)
-    scatter(continu_var(1,1:15:end), analysis.xr(1:15:end), 'Marker', mkr(mod(ii,length(mkr))),...
-        'MarkerEdgeColor', mc{mod(ii,size(mc,1))}, 'MarkerFaceColor', mc{mod(ii,size(mc,1))}); % plot the experimental data
+    scatter(continu_var(1,1:15:end)*Options.Bcorrt, analysis.xr(1:15:end)/gw2, 'Marker', mkr(mod(ii,length(mkr))),...
+        'MarkerEdgeColor', mc{mod(ii,size(mc,1))}, 'MarkerFaceColor', mc{mod(ii,size(mc,1))}); % plot the experimental Re[chi]
 
     figure(chii_fig)
-    scatter(continu_var, medfilt1(analysis.xi), 'Marker', mkr(mod(ii,length(mkr))),...
-        'MarkerEdgeColor', mc{mod(ii,size(mc,1))}, 'MarkerFaceColor', mc{mod(ii,size(mc,1))}); % plot the experimental data
+    scatter(continu_var(1,:)*Options.Bcorrt, medfilt1(analysis.xi)/gw2, 'Marker', '.',...
+        'MarkerEdgeColor', mc{mod(ii,size(mc,1))}, 'MarkerFaceColor', mc{mod(ii,size(mc,1))}); % plot the experimental Im[chi]
 %     figure(chii_fig)
 %     plot(continu_var(1,1:15:end), analysis.xi(1:15:end), 'LineStyle', '-', 'Marker', mkr(mod(ii,length(mkr))),...
 %         'Color', mc{mod(ii,size(mc,1))}); % plot the experimental data
-
-    sim_file = sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_%4$.2e_4*GHz', temps(ii), theta, phi, gama);
+    if exist('freq_l','var') && exist('freq_h','var')
+        sim_file = sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_%4$.2e_%5$.3f-%6$.3fGHz',...
+            sim_temps(ii), theta, phi, gama, freq_l, freq_h);
+    else
+        sim_file = sprintf('%1$3.3fK_%2$.2fDg_%3$.1fDg_%4$.2e_*GHz', sim_temps(ii), theta, phi, gama);
+    end
     if Options.RPA == true
         sim_load = strcat("RPA_LiHoF4_", sim_file, ".mat");
         xlabr = 'Re[\chi^{zz}_{RPA}] (meV)';
         xlabi = 'Im[\chi^{zz}_{RPA}] (meV)';
     else
-        sim_load = strcat("chi0_LiHoF4_", sim_file, ".mat");
+        sim_load = strcat("chi_LiHoF4_", sim_file, ".mat");
         xlabr = 'Re[\chi^{zz}_{MF}] (meV)';
         xlabi = 'Im[\chi^{zz}_{MF}] (meV)';
     end
-    chi_file = dir(fullfile(sim_loc, sim_load)); % use parts unspecified file name
+    chi_file = dir(fullfile(sim_loc, sim_load)); % search for the specified file
     if isempty(chi_file)
-        disp('Cannot match the file, check the name and directory!')
+        disp(strcat("Cannot match the file: ", sim_load, ", check the name and the directory!"))
         break
     end
     load([sim_loc chi_file.name],'-mat','freq_total','fields','chi','unit'); % load calculated suscpetibilities
-    chiz = squeeze(chi(3,3,:,:));
+    chiz = squeeze(chi(3,3,:,:)); % add real unit (meV)
 
     % Convert the unit to meV
     if strcmp(unit, 'GHz')
-        ConvUnit = 1/Gh2mV;
+        ConvUnit = Gh2mV;
     elseif strcmp(unit, 'J')
-        ConvUnit = meV2J;
-    else
+        ConvUnit = J2meV;
+    elseif strcmp(unit, 'meV')
         ConvUnit = 1;
+    else
+        disp('Energy unit unrecognized!')
     end
     
-    [~, fidx] = min(abs(freq_total - fc));
+    % use resonant frequency trace for linear plot of susceptibility
+    fref = interp1(B0, f0, fields); % cavity resonance frequency
+    % fref = 4.9164.*ones(size(fields)); % fixed cavity resonance frequency
+    fidx = uint32.empty(length(fields),0); % resonant frequency indices
+    chir = double.empty(length(fields),0);
+    chii = double.empty(length(fields),0);
+    for jj = 1:length(fields)
+        [~, fidx(jj)] = min(abs(freq_total - fref(jj)));
+        chir(jj) = ELEf^2 .* real( chiz(fidx(jj),jj) ) ./ ConvUnit;
+        chii(jj) = ELEf^2 .* imag( chiz(fidx(jj),jj) ) ./ ConvUnit;
+    end
+%     [~, fidx] = min(abs(freq_total - fc)); % use simple cavity frequency
+    
     figure(chir_fig);
-    plot(fields, real(chiz(fidx,:))/ConvUnit, 'LineStyle', lin(1), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
+    plot(fields, chir, 'LineStyle', lin(1), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
+%     plot(fields, real(chiz(fidx,:))/ConvUnit, 'LineStyle', lin(1), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
 
     figure(chii_fig);
-    plot(fields, imag(chiz(fidx,:))/ConvUnit, 'LineStyle', lin(1), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
+    plot(fields, chii, 'LineStyle', lin(2), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
+%     plot(fields, imag(chiz(fidx,:))/ConvUnit, 'LineStyle', lin(1), 'Color', mc{mod(ii,size(mc,1))}, 'lineWidth', 1.5);
 end
 
 lgd_exp = string(exp_temps.*1000); % legends for experimental data
