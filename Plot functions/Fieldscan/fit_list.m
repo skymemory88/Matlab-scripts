092d4b52cc8caaeb8e96f378974930cd8f629763
skymemory88@hotmail.com <skymemory88@hotmail.com>
2023-07-12 12:45:33 +0100

    add OS dependent addressing

--- Plot functions/Fieldscan/fit_list.m
+++ Plot functions/Fieldscan/fit_list.m
@@ -1,5 +1,21 @@
 function [aux_files, location, cmapfig] = fit_list(temp)
-if temp == 0.10
+
+if strcmp(pathsep, ':')
+    platform = 'Unix';
+else
+    platform = 'Win';
+end
+switch platform
+    case 'Win' %for Windows
+        location = 'G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC239\';
+        divd = '\';
+    case 'Unix' % for Mac/Linux
+        location = ['/Users/yikaiyang/Library/CloudStorage/GoogleDrive-yikai.yang@epfl.ch/My Drive/',...
+            'File sharing/PhD program/Research projects/LiHoF4 project/Data/Experiment/LiHoF4/SC239/'];
+        divd = '/';
+end
+
+if any([temp == 0.10, temp == 0.15], 'all')
     aux_files(1) = "2021_07_0003_0.148K_1.01-4.89T";
     aux_files(2) = "";
     aux_files(3) = "2021_07_0003_0.148K_6.80-8.79T";
@@ -12,9 +28,9 @@ if temp == 0.10
 %     aux_files(6) = "2021_07_0003_0.148K_12.20-14.49T"; % alternative/additional fits
 %     aux_files(7) = "2021_07_0003_0.142K_12.80-17.00T";
     aux_files(7) = "2021_07_0003_0.142K_13.01-17.00T"; % alternative/additional fits
-    location = "G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC239\2021.07.04\";
+    location = [location, '2021.07.04', divd];
     cmapfig = 'LiHoF4_3.52-3.76GHz_17-0T_150mK_-25dBm_0dB.fig';
-elseif temp == 0.135
+elseif any([temp == 0.135, temp == 0.181], 'all')
     aux_files(1) = "2021_07_0004_0.167K_2.00-4.50T";
     aux_files(2) = "2021_07_0004_0.167K_5.40-7.15T";
 %     aux_files(2) = "2021_07_0004_0.167K_5.31-7.09T"; % alternative/additional fits
@@ -29,9 +45,9 @@ elseif temp == 0.135
 %     aux_files(6) = "2021_07_0004_0.167K_12.00-13.79T"; % alternative/additional fits
     aux_files(7) = "2021_07_0004_0.167K_12.71-17.00T";
 %     aux_files(7) = "2021_07_0004_0.167K_12.50-15.99T"; % alternative/additional fits
-    location = "G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC239\2021.07.05\";
+    location = [location, '2021.07.05', divd];
     cmapfig = 'LiHoF4_3.52-3.76GHz_17-0T_180mK_-25dBm_0dB.fig';
-elseif temp == 0.18
+elseif any([temp == 0.18, temp == 0.22], 'all')
     aux_files(1) = "2021_07_0006_0.201K_0.00-4.71T";
     aux_files(2) = "2021_07_0006_0.201K_5.29-7.16T";
 %     aux_files(2) = "2021_07_0006_0.201K_5.25-7.19T"; % alternative/additional fits
@@ -43,9 +59,9 @@ elseif temp == 0.18
     aux_files(6) = ""; % space filler
     aux_files(7) = "2021_07_0006_0.201K_12.71-15.99T";
 %     aux_files(7) = "2021_07_0006_0.201K_13.01-15.99T"; % alternative/additional fits
-    location = "G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC239\2021.07.07\";
+    location = [location, '2021.07.07', divd];
     cmapfig = 'LiHoF4_3.52-3.76GHz_0-17T_220mK_-25dBm_0dB.fig';
-elseif temp == 0.24
+elseif any([temp == 0.24, temp == 0.26], 'all')
     aux_files(1) = "2021_07_0007_0.258K_0.01-4.44T";
     aux_files(2) = "2021_07_0007_0.258K_5.51-7.19T";
 %     aux_files(2) = "2021_07_0007_0.258K_5.25-7.13T"; % alternative/additional fits
@@ -56,10 +72,10 @@ elseif temp == 0.24
     aux_files(6) = ""; % space filler
     aux_files(7) = "2021_07_0007_0.258K_12.61-15.99T";
 %     aux_files(7) = "2021_07_0007_0.258K_12.17-14.70T"; % alternative/additional fits
-    location = "G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC239\2021.07.08\";
+    location = [location, '2021.07.08', divd];
     cmapfig = 'LiHoF4_3.52-3.76GHz_0-17T_260mK_-25dBm_0dB.fig';
-elseif temp == 0.25
-    aux_files(1) = "2021_07_0008_0.291K_0.00-4.35T"; % 290 mK
+elseif any([temp == 0.25, temp == 0.30], 'all')
+    aux_files(1) = "2021_07_0008_0.291K_0.00-4.35T";
     aux_files(2) = "2021_07_0008_0.291K_5.31-7.09T";
 %     aux_files(2) = "2021_07_0008_0.291K_5.31-7.39T"; % alternative/additional fits
 %     aux_files(2) = "2021_07_0008_0.291K_5.30-7.33T"; % alternative/additional fits
@@ -70,7 +86,7 @@ elseif temp == 0.25
     aux_files(5) = ""; % space filler
     aux_files(6) = ""; % space filler
     aux_files(7) = "2021_07_0008_0.291K_13.01-15.00T";
-    location = "G:\My Drive\File sharing\PhD program\Research projects\LiHoF4 project\Data\Experiment\LiHoF4\SC239\2021.07.09\";
+    location = [location, '2021.07.09', divd];
     cmapfig = 'LiHoF4_3.52-3.76GHz_0-17T_300mK_-25dBm_0dB.fig';
 end
 end
