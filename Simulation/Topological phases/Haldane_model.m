7569101b05bfdc7c0da3701ea444c65a091bebe6
skymemory88@hotmail.com <skymemory88@hotmail.com>
2023-12-31 03:37:05 +0000

    update

--- Simulation/Topological phases/Haldane_model.m
+++ Simulation/Topological phases/Haldane_model.m
@@ -1,14 +1,24 @@
 % Haldane model (Tnn defaults to 1)
-mmin = 0;
+mmin = 0.0;
 mmax = 0.5;
-t2min = -0.5*mmax;
-t2max = +0.5*mmax;
-lattc = 1.0; % lattice constant
-chainL = 20;
- 
+t2min = -mmax;
+t2max = +mmax;
+phi = -pi/2; % hopping phase
+
+lattc.const = 1.0;
+lattc.L = 25;
+% unit vector
+lattc.a = {[1 0].*lattc.const;
+    [-1/2 -sqrt(3)/2].*lattc.const;
+    [-1/2 sqrt(3)/2].*lattc.const};
+
+lattc.b = {[0 -sqrt(3)].*lattc.const;
+     [3/2 +sqrt(3)/2].*lattc.const;
+     [-3/2 +sqrt(3)/2].*lattc.const};
+
 % k-vector
-kx = linspace(-pi/lattc, pi/lattc, 200);
-ky = linspace(-pi/lattc, pi/lattc, 200);
+kx = linspace(-pi/lattc.const, pi/lattc.const, 200);
+ky = linspace(-pi/lattc.const, pi/lattc.const, 200);
 [KX, KY] = meshgrid(kx,ky);
 
 fobj1 = figure('Position', [50, 100, 1200, 440]);
@@ -16,79 +26,110 @@ ax1 = subplot('Position', [0.1 0.2 0.35 0.75]);
 
 box on
 hold(ax1,'on');
-[Er, Hr] = kBands(kx, ky, lattc, 0, 0);
+[Er, Hr] = kBands(kx, ky, lattc, 0, 0, phi);
 uband = surf(ax1, KX, KY, squeeze(Er(:,:,1)));
 uband.EdgeColor = 'none';
 lband = surf(ax1,KX, KY, squeeze(Er(:,:,2)));
 lband.EdgeColor = 'none';
 xlabel(ax1, '$\vec{k}_x$','Interpreter','latex')
+xticks([-pi -pi/2 0 pi/2 pi])
+xticklabels({'-\pi','-\pi/2','0','\pi/2','\pi'})
 ylabel(ax1, '$\vec{k}_y$','Interpreter','latex')
+yticks([-pi -pi/2 0 pi/2 pi])
+yticklabels({'-\pi','-\pi/2','0','\pi/2','\pi'})
 zlabel(ax1, 'Energy')
-set(ax1,'FontSize',12)
+set(ax1,'FontSize',15)
+cmap = unique([[0 0 0];[zeros(20,1),linspace(0,0.5,20)',linspace(0.4,1,20)'];...
+[linspace(0,1,36)',0.5*ones(36,1),linspace(1,0,36)'];...
+[ones(30,1),linspace(0.5,1,30)',linspace(0,1,30)']],'rows');
+cmap = [flip(cmap,1); cmap];
+colormap(cmap)
 view(30,35)
 
 figure(fobj1);
 ax2 = subplot('Position', [0.6 0.2 0.35 0.75]);
 box on
 hold(ax2,'on');
-En = ribbon(squeeze(Hr(100,:,:,:)), chainL);
-cband = plot(KY(:,1), squeeze(En(:,:)), '-k');
-xlabel(ax1, '$\vec{k}_y$','Interpreter','latex')
-ylabel(ax1, '$E_n$','Interpreter','latex')
-set(ax2,'FontSize',12)
+En = band_ribbon(lattc, kx, squeeze(Hr(100,:,:,:)), 0, phi);
+cband = plot(KX(1,:), squeeze(En(:,:)), '-k');
+xlabel(ax2, '$\vec{k}_x$','Interpreter','latex')
+ylabel(ax2, '$E_n$','Interpreter','latex')
+set(ax2,'FontSize',15)
 
 slider1 = uicontrol('Parent', fobj1, 'Style', 'slider',...
-    'OuterPosition', [100 400 150 20], 'Value', 0, 'min', t2min, 'max', t2max);
-sld1.label = uicontrol('Parent',fobj1,'Style','text','Position', [250 400 40 20],...
+    'OuterPosition', [100 30 150 20], 'Value', 0.0, 'min', t2min, 'max', t2max);
+sld1.label = uicontrol('Parent',fobj1,'Style','text','Position',  [310 30 40 20],...
                 'String','T2/T1');
+box1 = uicontrol('Style', 'edit','Position', [250 30 50 20], 'String', num2str(slider1.Value));
             
 slider2 = uicontrol('Parent', fobj1, 'Style', 'slider',...
-    'OuterPosition', [100 380 150 20], 'Value', 0.25, 'min', mmin, 'max', mmax);
-sld2.label = uicontrol('Parent',fobj1,'Style','text', 'Position',[250 380 40 20],...
+    'OuterPosition', [100 10 150 20], 'Value', 0.0, 'min', mmin, 'max', mmax);
+sld2.label = uicontrol('Parent',fobj1,'Style','text', 'Position', [310 10 50 20],...
                 'String','Mass/T1');
+box2 = uicontrol('Style', 'edit','Position', [250 10 50 20], 'String', num2str(slider2.Value));
             
-slider1.Callback = @(es1,ed) band_update({uband lband cband}, kx, ky, lattc,...
-    slider2.Value, es1.Value, chainL);
-slider2.Callback = @(es2,ed) band_update({uband lband cband}, kx, ky, lattc,...
-    es2.Value, slider1.Value, chainL);
+slider1.Callback = @(es,ed) band_update({uband lband cband}, kx, ky, lattc,...
+    slider2.Value, es.Value, phi, box1, box2);
+% box1.Callback = @(es,ed) band_update({uband lband cband}, kx, ky, lattc,...
+%     slider2.Value, str2double(es.Value), phi, chainL, box1, box2);
 
-function band_update(figs, kx, ky, lattc, mass, tnnn, NN)
-[Er, Hr] = kBands(kx, ky, lattc, mass, tnnn);
-En = ribbon(squeeze(Hr(100,:,:,:)), NN);
+slider2.Callback = @(es,ed) band_update({uband lband cband}, kx, ky, lattc,...
+    es.Value, slider1.Value, phi, box1, box2);
+% box2.Callback = @(es,ed) band_update({uband lband cband}, kx, ky, lattc,...
+%     str2double(es.Value), slider1.Value, phi, chainL, box1, box2);
+
+function band_update(figs, kx, ky, lattc, mass, tnnn, phi, box1, box2)
+set(box1, 'String', num2str(tnnn));
+set(box2, 'String', num2str(mass));
+[Er, Hr] = kBands(kx, ky, lattc, mass, tnnn, phi);
+En = band_ribbon(lattc, kx, squeeze(Hr(100,:,:,:)), tnnn, phi);
 figs{1}.ZData = Er(:,:,1);
 figs{2}.ZData = Er(:,:,2);
 for ii = 1:size(figs{3},1)
-    figs{3}(ii).YData = squeeze(En(:,ii));
+    figs{3}(ii).YData = real(squeeze(En(:,ii)));
 end
 drawnow
 end
 
-function En = ribbon(Hr, NN)
+function En = band_ribbon(lattc, kv, Hr, tnnn, phi)
+% unit vector
+a = lattc.a;
+b = lattc.b;
+NN = lattc.L;
+
 En = double.empty(size(Hr,1), NN*2, 0);
 for ii = 1:size(Hr,1)
     hr = squeeze(Hr(ii,:,:));
-    ham = kron(eye(NN),hr);
-    ham(NN*2+3:NN*4+2:end) = 2;
-    ham = (ham'+ham)/2;
+    ham = kron(eye(NN), hr/2);
+    
+% NN hopping betwen cells
+    ham((NN+1)*2+1 : (2*NN+1)*4 : end) = cos([kv(ii) 0]*a{2}') + 1i*sin([kv(ii) 0]*a{2}'); % NN hopping betwen cells
+    ham((NN+1)*2+(2*NN+1)*2+1 : (2*NN+1)*4 : end) = cos([kv(ii) 0]*a{3}') + 1i*sin([kv(ii) 0]*a{3}'); % NN hopping betwen cells
+    
+    ham(3 : 4*(2*NN+1) : end-NN*2) = tnnn*exp(-1i*phi)*(cos([kv(ii) 0]*b{1}') + 1i*sin([kv(ii) 0]*b{1}')); % NNN hopping betwen cells
+    ham(3+2*NN+1 : 4*(2*NN+1) : end) = tnnn*exp(-1i*phi)*(cos([kv(ii) 0]*b{1}') + 1i*sin([kv(ii) 0]*b{1}')); % NNN hopping betwen cells
+    ham(3+2*(2*NN+1) : 4*(2*NN+1) : end) = tnnn*exp(-1i*phi)*(cos([kv(ii) 0]*b{3}') + 1i*sin([kv(ii) 0]*b{3}')); % NNN hopping betwen cells
+    ham(3+3*(2*NN+1) : 4*(2*NN+1) : end) = tnnn*exp(-1i*phi)*(cos([kv(ii) 0]*b{3}') + 1i*sin([kv(ii) 0]*b{3}')); % NNN hopping betwen cells
+
+    ham = ham' + ham; % alternative way of constructing the hamiltonian
+    ham(1) = ham(1)-tnnn*sin(phi)*sin(phi)*sin([kv(ii) 0]*a{2}')...
+        - tnnn*sin(phi)*sin([kv(ii) 0]*a{3}'); % account for missing bonds at the edges
+    ham(end) = ham(end)-tnnn*sin(phi)*sin(phi)*sin([kv(ii) 0]*a{2}')...
+        - tnnn*sin(phi)*sin([kv(ii) 0]*a{3}'); % account for missing bonds at the edges
     [~, ee] = eig(ham);
-    En(ii,:,1) = diag(ee);
+    En(ii,:,1) = real(diag(ee));
 end
 end
 
-function [Er, Hr] = kBands(kx, ky, lattc, mass, tnnn)
+function [Er, Hr] = kBands(kx, ky, lattc, mass, tnnn, phi)
 % Pauli matrices
 sigx = [0   1;  1   0];
 sigy = [0 -1i;  1i  0];
 sigz = [1   0;  0  -1];
 
 % unit vector
-a = {[1 0].*lattc;
-    [-1/2 -sqrt(3)/2].*lattc;
-    [-1/2 sqrt(3)/2].*lattc};
-
-b = {[0 -sqrt(3)].*lattc;
-     [3/2 +sqrt(3)/2].*lattc;
-     [-3/2 +sqrt(3)/2].*lattc};
+a = lattc.a;
+b = lattc.b;
 
 Hr = double.empty(length(kx), length(ky), 2, 2, 0);
 Er = double.empty(length(kx), length(ky), 2, 0);
@@ -96,17 +137,19 @@ for ii = 1:length(kx)
     for jj = 1:length(ky)
         kv = [kx(ii) ky(jj)]; % k-vector
         
-        Hr1 = 0; Hr2 = 0; Hr3 = 0;
+        Hr1 = 0; Hr2 = 0;
         for kk = 1:length(a) % N.N. contribution
             Hr1 = Hr1 + cos(kv*a{kk}');
             Hr2 = Hr2 + sin(kv*a{kk}');
         end
-        
+        Hr3 = 0; Hr0 = 0;
         for kk = 1:length(b) % N.N.N. contribution
-            Hr3 = Hr3 + 2*tnnn*sin(kv*b{kk}');
+            Hr0 = Hr0 + 2*tnnn*cos(phi)*cos(kv*b{kk}');
+            Hr3 = Hr3 - 2*tnnn*sin(phi)*sin(kv*b{kk}');
         end
         
-        Hr(ii,jj,:,:,1) = (sigx.*Hr1 - sigy.*Hr2) + sigz*(mass + Hr3); % total hamiltonian
+        Hr(ii,jj,:,:,1) = (Hr1.*sigx - Hr2.*sigy) + (mass + Hr3).*sigz...
+            + Hr0.*eye(2); % total hamiltonian
         [~, ee] = eig(squeeze(Hr(ii,jj,:,:,1)));
         Er(ii,jj,:,1) = diag(ee);
     end
--- Simulation/Topological phases/SSH_model.m
+++ Simulation/Topological phases/SSH_model.m
@@ -1,9 +1,12 @@
 % SSH model
 nn = 200; % half length of the chain
 Options.fluc = 'none'; % hopping amplitude fluctuation
-Options.plot = true;
-Options.pdc = true;
+Options.plot = true; % Interactive plots
+Options.phase = true; % phase diagram
+Options.pdc = false; % periodic boundary condition
+Options.defc = true; % add a defect to the middle of the chain
 
+% Create a figure object for the interactive plots
 fObj1 = figure('WindowState', 'normal', 'Position', [50, 100, 1200, 440]);
 ax1 = subplot('Position', [0.1 0.1 0.35 0.75]);
 box on
@@ -16,6 +19,7 @@ wplot = plot(ax1, 0:N-1, real(sum(conj(wvr)'*wvr)),'--');
 ylabel('\psi(r)')
 xlabel('Site index')
 
+% Plot the band structure
 figure(fObj1);
 ax2 = subplot('Position', [0.6 0.1 0.35 0.75]);
 box on
@@ -28,12 +32,28 @@ ylabel('Energy')
 xlabel('Momentum')
 
 slider = uicontrol('Parent', fObj1, 'Style', 'slider',...
-    'OuterPosition', [100 400 150 20], 'Value', 0, 'min', 0, 'max', 2);
-sld.label = uicontrol('Parent',fObj1,'Style','text','Position', [250 400 40 20],...
+    'OuterPosition', [100 10 150 20], 'Value', 0, 'min', 0, 'max', 2);
+sld.label = uicontrol('Parent',fObj1,'Style','text','Position', [250 10 40 20],...
                 'String','J2/J1');
 
 slider.Callback = @(es1,ed) band_update([eplot wplot rband lband], nn, es1.Value, Options);
 
+if Options.phase == true
+    phase = figure;
+    hold on
+    box on
+    nn = 12;
+    v = 0:0.1:2; % range of inter-cell hoping amplitude
+    En = double.empty(2*nn,length(v),0);
+    for ii = 1:length(v)
+       [En(:,ii,1),~,~] = SSHband(nn, v(ii), Options);
+    end
+    plot(v,squeeze(En),'-k');
+    xlabel('J_2/J_1 ratio')
+    ylabel('Energy (arb. unit)')
+end
+
+% function to update the interactive plots
 function band_update(figs, nn, v, Options)
     [En, ~, wvr] = SSHband(nn, v, Options);
     figs(1).YData = En;
@@ -43,6 +63,7 @@ function band_update(figs, nn, v, Options)
     drawnow
 end
 
+% claculate the bands (nn: half chain length, v: inter-cell bond)
 function [En, N, wvr] = SSHband(nn, v, Options)
 % 1D SSH model
 N = 2*nn; % force the number of sites to be even
@@ -56,6 +77,16 @@ J(end) = 1.0; % intra-cell hopping
 for ii = 1:sp
     Hr(ii*(N+1) : sp*(N+1) : end) = J(ii);
     Hr(2+(ii-1)*(N+1) : sp*(N+1) : end) = J(ii);
+end
+
+if Options.defc == true
+    mid = round(length(Hr)/2);
+    utemp = Hr(mid,mid+1);
+    ltemp = Hr(mid+1,mid);
+    Hr(mid, mid+1) = Hr(mid+1,mid+2);
+    Hr(mid+1, mid) = Hr(mid+2,mid+1);
+    Hr(mid+1,mid+2) = utemp;
+    Hr(mid+2,mid+1) = ltemp;
 end
 
 switch Options.fluc
